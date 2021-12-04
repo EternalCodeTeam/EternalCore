3a784208b1ba7cafad5b26fb938a1ae0b207508b
fertiz <71133191+zitreF@users.noreply.github.com>
2021-12-04 14:41:54 +0100

    kick

--- src/main/java/com/eternalcode/core/listeners/PlayerChatListener.java
+++ src/main/java/com/eternalcode/core/listeners/PlayerChatListener.java
@@ -27,9 +27,7 @@ public class PlayerChatListener implements Listener {
 
     @EventHandler
     public void onPlayerKick(PlayerKickEvent event) {
-        if (event.reason().contains(Component.text("disconnect.spam"))) {
-            event.setCancelled(true);
-        }
+        event.setCancelled(event.reason().contains(Component.text("disconnect.spam"))); 
     }
 
     @EventHandler
