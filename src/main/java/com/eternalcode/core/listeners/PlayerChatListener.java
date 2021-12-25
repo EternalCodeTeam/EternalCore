23cf7e232789363b1bec1cfdc17bf3c27e1f622c
vLucky <65517973+vLuckyyy@users.noreply.github.com>
2021-12-25 17:18:57 +0100

    Simplify PlayerChatListener, add comment to PlaceholderUtils

--- src/main/java/com/eternalcode/core/listeners/PlayerChatListener.java
+++ src/main/java/com/eternalcode/core/listeners/PlayerChatListener.java
@@ -8,18 +8,20 @@ import com.eternalcode.core.command.implementations.ChatCommand;
 import com.eternalcode.core.utils.ChatUtils;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerChatListener implements Listener {
     @EventHandler
     public void onPlayerChat(AsyncChatEvent event) {
-        if (ChatCommand.chatMuted && !event.getPlayer().hasPermission("eternalcore.chat.bypass")) {
-            event.getPlayer().sendMessage(ChatUtils.color("Chat jest obecnie wyłączony"));
+        Player player = event.getPlayer();
+
+        if (ChatCommand.chatMuted && !player.hasPermission("eternalcore.chat.bypass")) {
+            player.sendMessage(ChatUtils.color("Chat jest obecnie wyłączony"));
             event.setCancelled(true);
             return;
         }
