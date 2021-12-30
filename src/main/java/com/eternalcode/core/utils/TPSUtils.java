aedddcde730619e56b9c56b771b8e779c5f0f447
vLucky <65517973+vLuckyyy@users.noreply.github.com>
2021-12-30 04:36:11 +0100

    Update TPSUtils.java

--- src/main/java/com/eternalcode/core/utils/TPSUtils.java
+++ src/main/java/com/eternalcode/core/utils/TPSUtils.java
@@ -11,6 +11,8 @@ public final class TPSUtils {
         return Arrays.stream(Bukkit.getTPS()).mapToObj(TPSUtils::format).collect(Collectors.joining(", "));
     }
 
+
+    // TODO: MOVE COLORS TO CONFIG
     private static String format(double tps) {
         return ((tps > 18.0) ? ChatColor.GREEN : (tps > 16.0) ? ChatColor.YELLOW : ChatColor.RED)
             + ((tps > 20.0) ? "*" : "") + Math.min(Math.round(tps * 100.0) / 100.0, 20.0);
