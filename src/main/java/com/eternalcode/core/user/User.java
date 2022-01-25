c825069746d48aa63e8a8b0fa73e8a183d1944cf
Rollczi <ndejlich5@gmail.com>
2022-01-25 14:47:47 +0100

    Mark bukkit&kyori dependency in User and UserService as @Deprecated.

--- src/main/java/com/eternalcode/core/user/User.java
+++ src/main/java/com/eternalcode/core/user/User.java
@@ -5,10 +5,10 @@
 package com.eternalcode.core.user;
 
 import com.eternalcode.core.chat.ChatUtils;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.title.Title;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import net.kyori.adventure.text.Component; // to remove
+import net.kyori.adventure.title.Title; // to remove
+import org.bukkit.Bukkit; // to remove
+import org.bukkit.entity.Player; // to remove
 import panda.std.Option;
 import panda.utilities.StringUtils;
 
@@ -36,18 +36,22 @@ public class User {
         return uuid;
     }
 
+    @Deprecated
     public void ifOnline(Consumer<Player> playerConsumer) {
         Option.of(Bukkit.getPlayer(name)).peek(playerConsumer);
     }
 
+    @Deprecated
     public void message(String message) {
         ifOnline(player -> player.sendMessage(ChatUtils.color(message)));
     }
 
+    @Deprecated
     public void actionBar(String message) {
         ifOnline(player -> player.sendActionBar(Component.text(ChatUtils.color(message))));
     }
 
+    @Deprecated
     public void title(String title, String subTitle, long in, long stay, long out) {
         Function<Long, Duration> calcDuration = integer -> Duration.of(integer * 50, ChronoUnit.MILLIS);
         Title.Times times = Title.Times.of(calcDuration.apply(in), calcDuration.apply(stay), calcDuration.apply(out));
@@ -56,10 +60,12 @@ public class User {
         ifOnline(player -> player.showTitle(titleComponent));
     }
 
+    @Deprecated
     public void title(String title, long in, long stay, long out) {
         title(title, StringUtils.EMPTY, in, stay, out);
     }
 
+    @Deprecated
     public void subTitle(String subTitle, long in, long stay, long out) {
         title(StringUtils.EMPTY, subTitle, in, stay, out);
     }
