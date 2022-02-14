17bc547881f440e09defff1c2c193366503811ba
ItsFertiZ <71133191+ItsFertiZ@users.noreply.github.com>
2022-02-14 14:31:12 +0100

    ping and enchant commands!

--- src/main/java/com/eternalcode/core/EternalCore.java
+++ src/main/java/com/eternalcode/core/EternalCore.java
@@ -13,36 +13,7 @@ import com.eternalcode.core.command.argument.MessageActionArgument;
 import com.eternalcode.core.command.argument.PlayerArgument;
 import com.eternalcode.core.command.argument.StringPlayerArgument;
 import com.eternalcode.core.command.bind.PlayerSenderBind;
-import com.eternalcode.core.command.implementations.AdminChatCommand;
-import com.eternalcode.core.command.implementations.AlertCommand;
-import com.eternalcode.core.command.implementations.AnvilCommand;
-import com.eternalcode.core.command.implementations.CartographyTableCommand;
-import com.eternalcode.core.command.implementations.ChatCommand;
-import com.eternalcode.core.command.implementations.ClearCommand;
-import com.eternalcode.core.command.implementations.DisposalCommand;
-import com.eternalcode.core.command.implementations.EnderchestCommand;
-import com.eternalcode.core.command.implementations.EternalCoreCommand;
-import com.eternalcode.core.command.implementations.FeedCommand;
-import com.eternalcode.core.command.implementations.FlyCommand;
-import com.eternalcode.core.command.implementations.GamemodeCommand;
-import com.eternalcode.core.command.implementations.GiveCommand;
-import com.eternalcode.core.command.implementations.GodCommand;
-import com.eternalcode.core.command.implementations.GrindstoneCommand;
-import com.eternalcode.core.command.implementations.HatCommand;
-import com.eternalcode.core.command.implementations.HealCommand;
-import com.eternalcode.core.command.implementations.HelpOpCommand;
-import com.eternalcode.core.command.implementations.InventoryOpenCommand;
-import com.eternalcode.core.command.implementations.KillCommand;
-import com.eternalcode.core.command.implementations.RepairCommand;
-import com.eternalcode.core.command.implementations.ScoreboardCommand;
-import com.eternalcode.core.command.implementations.SetSpawnCommand;
-import com.eternalcode.core.command.implementations.SkullCommand;
-import com.eternalcode.core.command.implementations.SpawnCommand;
-import com.eternalcode.core.command.implementations.SpeedCommand;
-import com.eternalcode.core.command.implementations.StonecutterCommand;
-import com.eternalcode.core.command.implementations.TeleportCommand;
-import com.eternalcode.core.command.implementations.WhoIsCommand;
-import com.eternalcode.core.command.implementations.WorkbenchCommand;
+import com.eternalcode.core.command.implementations.*;
 import com.eternalcode.core.command.message.PermissionMessage;
 import com.eternalcode.core.configuration.ConfigurationManager;
 import com.eternalcode.core.configuration.implementations.CommandsConfiguration;
@@ -192,7 +163,8 @@ public class EternalCore extends JavaPlugin {
                 RepairCommand.class,
                 GiveCommand.class,
                 SetSpawnCommand.class,
-                SpawnCommand.class
+                SpawnCommand.class,
+                PingCommand.class
             )
             .register();
 
