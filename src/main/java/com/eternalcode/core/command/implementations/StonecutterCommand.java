d2b862e91d42578fba2071ba345bec94624fad39
Osnixer <Osnixeroo@gmail.com>
2022-02-22 23:51:56 +0100

    Add player-only message

--- src/main/java/com/eternalcode/core/command/implementations/StonecutterCommand.java
+++ src/main/java/com/eternalcode/core/command/implementations/StonecutterCommand.java
@@ -5,6 +5,8 @@
 package com.eternalcode.core.command.implementations;
 
 import com.eternalcode.core.command.argument.PlayerArgument;
+import com.eternalcode.core.configuration.implementations.MessagesConfiguration;
+import com.eternalcode.core.utils.ChatUtils;
 import dev.rollczi.litecommands.annotations.Arg;
 import dev.rollczi.litecommands.annotations.Execute;
 import dev.rollczi.litecommands.annotations.Handler;
@@ -18,6 +20,12 @@ import panda.std.Option;
 @Permission("eternalcore.command.workbench")
 public class StonecutterCommand {
 
+    private final MessagesConfiguration messages;
+
+    public StonecutterCommand(MessagesConfiguration messages) {
+        this.messages = messages;
+    }
+
     @Execute
     public void execute(CommandSender sender, @Arg(0) @Handler(PlayerArgument.class) Option<Player> playerOption) {
         if (playerOption.isEmpty()) {
@@ -26,7 +34,7 @@ public class StonecutterCommand {
                 return;
             }
 
-            //TODO: send NO CONSOLE
+            sender.sendMessage(ChatUtils.color(this.messages.argumentSection.onlyPlayer));
             return;
         }
 
