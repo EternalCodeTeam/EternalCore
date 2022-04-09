8becb2319c0e9f1a4fb78a9a132594fc694d303a
vLuckyDev <vluckyyy.biznes@gmail.com>
2022-04-09 22:13:28 +0200

    Add NoticeService for Listeners
    Co-Authored-By: VelvetDuck <44951860+VelvetDuck@users.noreply.github.com>

--- eternalcore/src/main/java/com/eternalcode/core/listener/player/PlayerDeathListener.java
+++ eternalcore/src/main/java/com/eternalcode/core/listener/player/PlayerDeathListener.java
@@ -1,25 +1,35 @@
 package com.eternalcode.core.listener.player;
 
+import com.eternalcode.core.chat.notification.NoticeService;
+import com.eternalcode.core.chat.notification.NoticeType;
 import com.eternalcode.core.configuration.ConfigurationManager;
 import com.eternalcode.core.configuration.implementations.PluginConfiguration;
-import com.eternalcode.core.utils.ChatUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import panda.utilities.StringUtils;
 
 public class PlayerDeathListener implements Listener {
 
+    private final NoticeService noticeService;
     private final PluginConfiguration config;
 
-    public PlayerDeathListener(ConfigurationManager configurationManager){
+    public PlayerDeathListener(NoticeService noticeService, ConfigurationManager configurationManager) {
+        this.noticeService = noticeService;
         this.config = configurationManager.getPluginConfiguration();
     }
 
     @EventHandler
-    public void onDeath(PlayerDeathEvent event) {
+    public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
 
-       // event.deathMessage(ChatUtils.component(this.config.eventMessage.deathMessage.replace("{PLAYER}", player.getName())));
+        event.setDeathMessage(StringUtils.EMPTY);
+
+        noticeService.notice()
+            .notice(NoticeType.CHAT, messages -> config.eventMessage.deathMessage)
+            .placeholder("{PLAYER}", player.getName())
+            .all()
+            .send();
     }
 }
