ad4b6cda0b05de562dac4c55969e483e5cdc1065
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2022-12-26 19:35:24 +0100

    LocaleComposerTest

---
+++ eternalcore/src/test/java/com/eternalcode/core/configuration/language/LocaleComposerTest.java
@@ -0,0 +1,31 @@
+package com.eternalcode.core.configuration.language;
+
+import org.junit.jupiter.api.Test;
+import panda.std.Result;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocaleComposerTest {
+
+    LocaleComposer composer = new LocaleComposer();
+
+    @Test
+    public void testDeserialize() {
+        String source = "en";
+        Result<Locale, Exception> result = composer.deserialize(source);
+
+        assertTrue(result.isOk());
+        assertEquals("en", result.get().getLanguage());
+    }
+
+    @Test
+    public void testSerialize() {
+        Locale entity = new Locale("en");
+        Result<String, Exception> result = composer.serialize(entity);
+
+        assertTrue(result.isOk());
+        assertEquals("en", result.get());
+    }
+}
