ba0a27a3325075e75a514623e44691387e6577c3
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2022-12-26 20:32:07 +0100

    Add locations configuration comment

--- eternalcore/src/main/java/com/eternalcode/core/configuration/implementation/LocationsConfiguration.java
+++ eternalcore/src/main/java/com/eternalcode/core/configuration/implementation/LocationsConfiguration.java
@@ -2,6 +2,7 @@ package com.eternalcode.core.configuration.implementation;
 
 import com.eternalcode.core.configuration.ReloadableConfig;
 import com.eternalcode.core.shared.Position;
+import net.dzikoysk.cdn.entity.Description;
 import net.dzikoysk.cdn.source.Resource;
 import net.dzikoysk.cdn.source.Source;
 
@@ -11,6 +12,7 @@ import java.util.Map;
 
 public class LocationsConfiguration implements ReloadableConfig {
 
+    @Description("Don't touch this")
     public Position spawn = new Position(0, 0, 0, 0.0f, 0.0f, "world");
 
     public Map<String, Position> warps = new HashMap<>();
