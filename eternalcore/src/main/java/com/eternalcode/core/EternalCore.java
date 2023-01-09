82717f9192caabcf156edd37d62d88d6389f6f41
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-01-09 10:36:38 +0100

    Gh-261 Apply space before quote

--- eternalcore/src/main/java/com/eternalcode/core/EternalCore.java
+++ eternalcore/src/main/java/com/eternalcode/core/EternalCore.java
@@ -336,7 +336,7 @@ public class EternalCore extends JavaPlugin {
             .argument(User.class,                   new UserArgument(this.viewerProvider, this.translationManager, server, this.userManager))
             .argument(Player.class,                 new PlayerArgument(this.viewerProvider, this.translationManager, server))
             .argument(Player.class, "request",  new RequesterArgument(this.teleportRequestService, this.translationManager, this.viewerProvider, server))
-            .argument(Integer.class,"speed",    new SpeedArgument())
+            .argument(Integer.class, "speed",    new SpeedArgument())
 
             // multilevel Arguments (include optional)
             .argumentMultilevel(Location.class,     new LocationArgument())
