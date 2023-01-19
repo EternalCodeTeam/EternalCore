68ef712b47ca4b26f60ea2f3b50102a8642d3448
Karol Dronia <karol.dronia@gmail.com>
2023-01-19 22:12:44 +0100

    Remove redutant import in PlTranslation class

--- eternalcore/src/main/java/com/eternalcode/core/translation/implementation/PLTranslation.java
+++ eternalcore/src/main/java/com/eternalcode/core/translation/implementation/PLTranslation.java
@@ -4,7 +4,6 @@ import com.eternalcode.core.language.Language;
 import com.eternalcode.core.notification.NoticeType;
 import com.eternalcode.core.notification.Notification;
 import com.eternalcode.core.translation.AbstractTranslation;
-import com.j256.ormlite.stmt.query.Not;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import net.dzikoysk.cdn.entity.Contextual;
