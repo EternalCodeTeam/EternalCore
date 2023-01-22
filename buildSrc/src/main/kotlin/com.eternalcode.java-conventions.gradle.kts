a49d0759c0623aac3b516218f645bef7408586da
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-01-22 17:56:39 +0100

    Update com.eternalcode.java-conventions.gradle.kts

--- buildSrc/src/main/kotlin/com.eternalcode.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.eternalcode.java-conventions.gradle.kts
@@ -12,7 +12,7 @@ repositories {
     maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
     maven { url = uri("https://repository.minecodes.pl/releases") }
     maven { url = uri("https://repository.minecodes.pl/snapshots") }
-    maven { url = uri("https://repo.eternalcode.pl/releases") }
+    maven { url = uri("https://repo.alessiodp.com/releases/") }
     maven { url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/") }
 }
 
