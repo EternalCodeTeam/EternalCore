cbace4f941dc67854e81b2a3622eebf194d7fc28
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-02-06 01:44:09 +0100

    Remove or_sender from GodCommand

--- core/src/main/java/com/eternalcode/core/feature/essentials/GodCommand.java
+++ core/src/main/java/com/eternalcode/core/feature/essentials/GodCommand.java
@@ -3,11 +3,9 @@ package com.eternalcode.core.feature.essentials;
 import com.eternalcode.core.notification.NoticeService;
 import com.eternalcode.core.viewer.Viewer;
 import dev.rollczi.litecommands.argument.Arg;
-import dev.rollczi.litecommands.argument.By;
 import dev.rollczi.litecommands.command.execute.Execute;
 import dev.rollczi.litecommands.command.permission.Permission;
 import dev.rollczi.litecommands.command.route.Route;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @Route(name = "god", aliases = "godmode")
@@ -21,22 +19,27 @@ public class GodCommand {
     }
 
     @Execute
-    void execute(CommandSender sender, Viewer viewer, @Arg @By("or_sender") Player player) {
+    void execute(Player sender) {
+        sender.setInvulnerable(!sender.isInvulnerable());
+
+        this.noticeService.create()
+            .placeholder("{STATE}", translation -> sender.isInvulnerable() ? translation.format().enable() : translation.format().disable())
+            .notice(translation -> translation.player().godMessage())
+            .player(sender.getUniqueId())
+            .send();
+    }
+
+    @Execute(required = 1)
+    void execute(Viewer viewer, @Arg Player player) {
         player.setInvulnerable(!player.isInvulnerable());
 
-        this.noticeService
-            .create()
+        this.noticeService.create()
             .placeholder("{STATE}", translation -> player.isInvulnerable() ? translation.format().enable() : translation.format().disable())
             .notice(translation -> translation.player().godMessage())
             .player(player.getUniqueId())
             .send();
 
-        if (sender.equals(player)) {
-            return;
-        }
-
-        this.noticeService
-            .create()
+        this.noticeService.create()
             .placeholder("{STATE}", translation -> player.isInvulnerable() ? translation.format().enable() : translation.format().disable())
             .placeholder("{PLAYER}", player.getName())
             .notice(translation -> translation.player().godSetMessage())
