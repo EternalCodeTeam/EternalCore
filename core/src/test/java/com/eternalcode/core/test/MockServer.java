b582965e7f554e8f0f8580449b9a19113a56b83d
Rollczi <ndejlich5@gmail.com>
2023-03-04 01:42:17 +0100

    Add MockServer, test full lifecycle of User controlled by PrepareUserController.

---
+++ core/src/test/java/com/eternalcode/core/test/MockServer.java
@@ -0,0 +1,89 @@
+package com.eternalcode.core.test;
+
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MockServer {
+
+    private final Server server;
+    private final Map<UUID, Player> onlinePlayers = new HashMap<>();
+
+    private final List<Consumer<PlayerJoinEvent>> joinListeners = new ArrayList<>();
+    private final List<Consumer<PlayerQuitEvent>> quitListeners = new ArrayList<>();
+    private final List<Consumer<PlayerKickEvent>> kickListeners = new ArrayList<>();
+
+    public MockServer() {
+        this.server = mock(Server.class);
+
+        when(this.server.getPlayer(any(UUID.class))).thenReturn(null);
+    }
+
+    public Player joinPlayer(String name, UUID uuid) {
+        Player player = mock(Player.class);
+
+        when(player.getName()).thenReturn(name);
+        when(player.getUniqueId()).thenReturn(uuid);
+        when(this.server.getPlayer(eq(uuid))).then(invocation -> this.onlinePlayers.get(uuid));
+
+        this.onlinePlayers.put(uuid, player);
+
+        PlayerJoinEvent event = new PlayerJoinEvent(player, "Player " + name + " joined to the server!");
+
+        for (Consumer<PlayerJoinEvent> joinListener : this.joinListeners) {
+            joinListener.accept(event);
+        }
+
+        return player;
+    }
+
+    public void quitPlayer(Player player) {
+        this.onlinePlayers.remove(player.getUniqueId());
+
+        PlayerQuitEvent event = new PlayerQuitEvent(player, "Player " + player.getName() + " quit from the server!");
+
+        for (Consumer<PlayerQuitEvent> quitListener : this.quitListeners) {
+            quitListener.accept(event);
+        }
+    }
+
+    public void kickPlayer(Player player) {
+        this.onlinePlayers.remove(player.getUniqueId());
+
+        PlayerKickEvent event = new PlayerKickEvent(player, "Player " + player.getName() + " has been kicked!", "Kicked!");
+
+        for (Consumer<PlayerKickEvent> kickListener : this.kickListeners) {
+            kickListener.accept(event);
+        }
+    }
+
+    public void listenJoin(Consumer<PlayerJoinEvent> joinListener) {
+        this.joinListeners.add(joinListener);
+    }
+
+    public void listenQuit(Consumer<PlayerQuitEvent> quitListener) {
+        this.quitListeners.add(quitListener);
+    }
+
+    public void listenKick(Consumer<PlayerKickEvent> kickListener) {
+        this.kickListeners.add(kickListener);
+    }
+
+    public Server getServer() {
+        return this.server;
+    }
+}
--- core/src/test/java/com/eternalcode/core/user/PrepareUserControllerTest.java
+++ core/src/test/java/com/eternalcode/core/user/PrepareUserControllerTest.java
@@ -1,74 +1,61 @@
 package com.eternalcode.core.user;
 
+import com.eternalcode.core.test.MockServer;
+import com.eternalcode.core.user.client.ClientBukkitSettings;
 import com.eternalcode.core.user.client.ClientSettings;
-import org.bukkit.Server;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerKickEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import java.util.UUID;
 
+import static com.eternalcode.core.test.OptionAssertions.assertOptionEmpty;
+import static com.eternalcode.core.test.OptionAssertions.assertOptionPresent;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class PrepareUserControllerTest {
 
-    private static final UUID FAKE_UUID = UUID.randomUUID();
-    private static final String FAKE_NAME = "Martin";
+    private static final UUID PLAYER_UUID = UUID.randomUUID();
+    private static final String PLAYER_NAME = "Martin";
 
+    private MockServer mockServer;
     private UserManager userManager;
-    private PrepareUserController prepareUserController;
-
-    private AutoCloseable mocks;
-
-    @Mock
-    private Server server;
 
     @BeforeEach
     void setUp() {
-        this.mocks = MockitoAnnotations.openMocks(this);
+        this.mockServer = new MockServer();
         this.userManager = new UserManager();
-        this.prepareUserController = new PrepareUserController(this.userManager, this.server);
-    }
 
-    @AfterEach
-    void tearDown() throws Exception {
-        this.mocks.close();
+        PrepareUserController controller = new PrepareUserController(this.userManager, this.mockServer.getServer());
+
+        this.mockServer.listenJoin(controller::onJoin);
+        this.mockServer.listenQuit(controller::onQuit);
+        this.mockServer.listenKick(controller::onKick);
     }
 
     @Test
-    void createUserAndSetClientSettings() {
-        Player player = mock(Player.class);
+    @DisplayName("should create user and set ClientBukkitSettings for user")
+    void testJoinPlayer() {
+        assertOptionEmpty(this.userManager.getUser(PLAYER_UUID));
 
-        when(player.getUniqueId()).thenReturn(FAKE_UUID);
-        when(player.getName()).thenReturn(FAKE_NAME);
-
-        PlayerJoinEvent event = new PlayerJoinEvent(player, "fake join message");
-
-        User user = new User(FAKE_UUID, FAKE_NAME);
-        this.prepareUserController.onJoin(event);
+        this.mockServer.joinPlayer(PLAYER_NAME, PLAYER_UUID);
+        User user = assertOptionPresent(this.userManager.getUser(PLAYER_UUID));
 
         ClientSettings clientSettings = user.getClientSettings();
         assertTrue(clientSettings.isOnline());
+        assertInstanceOf(ClientBukkitSettings.class, clientSettings);
     }
 
     @Test
-    void removeClientSettingIfPlayerQuit() {
-        Player player = mock(Player.class);
+    @DisplayName("should create user and reset client settings to NONE after quit")
+    void testPlayerQuit() {
+        Player player = this.mockServer.joinPlayer(PLAYER_NAME, PLAYER_UUID);
+        User user = assertOptionPresent(this.userManager.getUser(PLAYER_UUID));
 
-        when(player.getUniqueId()).thenReturn(FAKE_UUID);
-        PlayerQuitEvent event = new PlayerQuitEvent(player, "fake quit message");
-
-        User user = new User(FAKE_UUID, FAKE_NAME);
-        this.prepareUserController.onQuit(event);
+        this.mockServer.quitPlayer(player);
 
         ClientSettings clientSettings = user.getClientSettings();
         assertTrue(clientSettings.isOffline());
@@ -76,17 +63,16 @@ class PrepareUserControllerTest {
     }
 
     @Test
-    void removeClientSettingIfPlayerKick() {
-        Player player = mock(Player.class);
+    @DisplayName("should create user and reset client settings to NONE after kick")
+    void testPlayerKick() {
+        Player player = this.mockServer.joinPlayer(PLAYER_NAME, PLAYER_UUID);
+        User user = assertOptionPresent(this.userManager.getUser(PLAYER_UUID));
 
-        when(player.getUniqueId()).thenReturn(FAKE_UUID);
-        PlayerKickEvent event = new PlayerKickEvent(player, "fake kick message", "fake kicker");
-
-        User user = new User(FAKE_UUID, FAKE_NAME);
-        this.prepareUserController.onKick(event);
+        this.mockServer.kickPlayer(player);
 
         ClientSettings clientSettings = user.getClientSettings();
         assertTrue(clientSettings.isOffline());
         assertEquals(ClientSettings.NONE, clientSettings);
     }
+
 }
