5078830548fd4f198043a559a9f59f575c5290af
Rollczi <ndejlich5@gmail.com>
2023-03-25 18:47:12 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 rootProject.name = "EternalCore"
 
+include("api")
 include(":core")
 include(":paper")
 
 
 project(":core").projectDir = file("core")
 project(":paper").projectDir = file("paper")
-include("api")
