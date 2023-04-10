ca307e48f09d8428cea28aee16a75f42257e4a13
Norbert Dejlich <49173834+Rollczi@users.noreply.github.com>
2023-04-10 19:07:27 +0200

    Update eternalcore-api/build.gradle.kts
    Co-authored-by: Jakubk15 <77227023+Jakubk15@users.noreply.github.com>

--- eternalcore-api/build.gradle.kts
+++ eternalcore-api/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
     api("org.panda-lang:expressible:1.3.1")
 }
