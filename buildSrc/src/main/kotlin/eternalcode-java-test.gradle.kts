6d3a31568adef8df8b996111c28deea876cd6723
Norbert Dejlich <49173834+Rollczi@users.noreply.github.com>
2023-04-10 16:51:35 +0200

    Update buildSrc/src/main/kotlin/eternalcode-java-test.gradle.kts
    Co-authored-by: Jakubk15 <77227023+Jakubk15@users.noreply.github.com>

--- buildSrc/src/main/kotlin/eternalcode-java-test.gradle.kts
+++ buildSrc/src/main/kotlin/eternalcode-java-test.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    testImplementation("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
+    testImplementation("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
     testImplementation("net.dzikoysk:cdn:1.14.4")
     testImplementation("org.codehaus.groovy:groovy-all:3.0.17")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
