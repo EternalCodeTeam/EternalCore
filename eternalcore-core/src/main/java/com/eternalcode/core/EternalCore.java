40237d61594a4acdf1d584c460100550467fc250
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2023-04-18 20:47:58 +0200

    change task time

--- eternalcore-core/src/main/java/com/eternalcode/core/EternalCore.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/EternalCore.java
@@ -436,7 +436,7 @@ class EternalCore implements EternalCoreApi {
         this.scheduler.timerSync(task, Duration.ofMillis(200), Duration.ofMillis(200));
 
         AfkTask afkTask = new AfkTask(this.afkService, server);
-        this.scheduler.timerSync(afkTask, Duration.ofMillis(1), Duration.ofMinutes(1));
+        this.scheduler.timerSync(afkTask, Duration.ofMinutes(1), Duration.ofMinutes(1));
 
         // bStats metrics
         Metrics metrics = new Metrics((JavaPlugin) plugin, 13964);
