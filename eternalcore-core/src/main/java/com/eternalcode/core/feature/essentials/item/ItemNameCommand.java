933b7fe8e1cdd8b601e8415fe29c7057832294bf
Piotr Zych <77621271+Osnixer@users.noreply.github.com>
2023-05-22 22:37:18 +0200

    Update eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/item/ItemNameCommand.java
    Co-authored-by: Norbert Dejlich <49173834+Rollczi@users.noreply.github.com>

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/item/ItemNameCommand.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/item/ItemNameCommand.java
@@ -2,7 +2,6 @@ package com.eternalcode.core.feature.essentials.item;
 
 import com.eternalcode.annotations.scan.command.DescriptionDocs;
 import com.eternalcode.core.notification.NoticeService;
-import com.eternalcode.core.util.AdventureUtil;
 import com.eternalcode.core.util.legacy.Legacy;
 import dev.rollczi.litecommands.argument.joiner.Joiner;
 import dev.rollczi.litecommands.command.amount.Min;
