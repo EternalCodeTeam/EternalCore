039b29c38105e264992bff95dbfd465975e9223b
DMK <dominiks8318@gmail.com>
2023-05-25 13:40:34 +0200

    Variable renamed from amount to speed

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/speed/SpeedCommand.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/speed/SpeedCommand.java
@@ -22,80 +22,80 @@ public class SpeedCommand {
     }
 
     @Execute(required = 1)
-    @DescriptionDocs(description = "Set speed depending on whether you flying or walking by specified amount", arguments = "<amount>")
-    void execute(Player player, @Arg @By(SpeedArgument.KEY) Integer amount) {
-        this.setSpeed(player, amount);
+    @DescriptionDocs(description = "Set speed depending on whether you flying or walking by specified amount", arguments = "<speed>")
+    void execute(Player player, @Arg @By(SpeedArgument.KEY) Integer speed) {
+        this.setSpeed(player, speed);
 
         this.noticeService.create()
                 .notice(translation -> player.isFlying() ? translation.player().speedFlySet() : translation.player().speedWalkSet())
-                .placeholder("{SPEED}", String.valueOf(amount))
+                .placeholder("{SPEED}", String.valueOf(speed))
                 .player(player.getUniqueId())
                 .send();
     }
 
     @Execute(required = 2)
-    @DescriptionDocs(description = "Set speed depending on whether the player is flying or walking by specified amount", arguments = "<amount> <player>")
-    void execute(Viewer viewer, @Arg @By(SpeedArgument.KEY) Integer amount, @Arg Player target) {
-        this.setSpeed(target, amount);
+    @DescriptionDocs(description = "Set speed depending on whether the player is flying or walking by specified amount", arguments = "<speed> <player>")
+    void execute(Viewer viewer, @Arg @By(SpeedArgument.KEY) Integer speed, @Arg Player target) {
+        this.setSpeed(target, speed);
 
         this.noticeService.create()
                 .notice(translation -> target.isFlying() ? translation.player().speedFlySet() : translation.player().speedWalkSet())
-                .placeholder("{SPEED}", String.valueOf(amount))
+                .placeholder("{SPEED}", String.valueOf(speed))
                 .player(target.getUniqueId())
                 .send();
 
         this.noticeService.create()
                 .notice(translation -> target.isFlying() ? translation.player().speedFlySetBy() : translation.player().speedWalkSetBy())
                 .placeholder("{PLAYER}", target.getName())
-                .placeholder("{SPEED}", String.valueOf(amount))
+                .placeholder("{SPEED}", String.valueOf(speed))
                 .viewer(viewer)
                 .send();
     }
 
     @Execute(required = 2)
-    @DescriptionDocs(description = "Set speed of walking or flying to specified amount", arguments = "<type> <amount>")
-    void execute(Player player, @Arg SpeedType speedType, @Arg @By(SpeedArgument.KEY) Integer amount) {
-        this.setSpeed(player, speedType, amount);
+    @DescriptionDocs(description = "Set speed of walking or flying to specified amount", arguments = "<type> <speed>")
+    void execute(Player player, @Arg SpeedType speedType, @Arg @By(SpeedArgument.KEY) Integer speed) {
+        this.setSpeed(player, speedType, speed);
 
         this.noticeService.create()
                 .notice(translation -> speedType == SpeedType.WALK ? translation.player().speedWalkSet() : translation.player().speedFlySet())
-                .placeholder("{SPEED}", String.valueOf(amount))
+                .placeholder("{SPEED}", String.valueOf(speed))
                 .player(player.getUniqueId())
                 .send();
     }
 
     @Execute(required = 3)
-    @DescriptionDocs(description = "Set speed of walking or flying to specified amount and player", arguments = "<type> <amount> <player>")
-    void execute(Viewer viewer, @Arg SpeedType speedType, @Arg @By(SpeedArgument.KEY) Integer amount, @Arg Player target) {
-        this.setSpeed(target, speedType, amount);
+    @DescriptionDocs(description = "Set speed of walking or flying to specified amount and player", arguments = "<type> <speed> <player>")
+    void execute(Viewer viewer, @Arg SpeedType speedType, @Arg @By(SpeedArgument.KEY) Integer speed, @Arg Player target) {
+        this.setSpeed(target, speedType, speed);
 
         this.noticeService.create()
                 .notice(translation -> speedType == SpeedType.WALK ? translation.player().speedWalkSet() : translation.player().speedFlySet())
-                .placeholder("{SPEED}", String.valueOf(amount))
+                .placeholder("{SPEED}", String.valueOf(speed))
                 .player(target.getUniqueId())
                 .send();
 
         this.noticeService.create()
                 .notice(translation -> speedType == SpeedType.WALK ? translation.player().speedWalkSetBy() : translation.player().speedFlySetBy())
                 .placeholder("{PLAYER}", target.getName())
-                .placeholder("{SPEED}", String.valueOf(amount))
+                .placeholder("{SPEED}", String.valueOf(speed))
                 .viewer(viewer)
                 .send();
     }
 
-    void setSpeed(Player player, int amount) {
+    void setSpeed(Player player, int speed) {
         if (player.isFlying()) {
-            player.setFlySpeed(amount / 10.0f);
+            player.setFlySpeed(speed / 10.0f);
         }
         else {
-            player.setWalkSpeed(amount / 10.0f);
+            player.setWalkSpeed(speed / 10.0f);
         }
     }
 
-    void setSpeed(Player player, SpeedType speedType, int amount) {
+    void setSpeed(Player player, SpeedType speedType, int speed) {
         switch (speedType) {
-            case WALK -> player.setWalkSpeed(amount / 10.0f);
-            case FLY -> player.setFlySpeed(amount / 10.0f);
+            case WALK -> player.setWalkSpeed(speed / 10.0f);
+            case FLY -> player.setFlySpeed(speed / 10.0f);
             default -> throw new IllegalStateException("Unexpected value: " + speedType);
         }
     }
