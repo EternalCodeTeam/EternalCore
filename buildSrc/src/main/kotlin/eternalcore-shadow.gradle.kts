c98e36fe4948042fbb3da1439459a809339f08e6
Rollczi <ndejlich5@gmail.com>
2023-07-09 10:32:03 +0200

    Normalize gradle script.

--- buildSrc/src/main/kotlin/eternalcore-shadow.gradle.kts
+++ buildSrc/src/main/kotlin/eternalcore-shadow.gradle.kts
@@ -8,20 +8,8 @@ val extension = extensions.create<EternalShadowExtension>("eternalShadow")
 
 afterEvaluate {
     dependencies {
-        extension.implementationDependencies.forEach {
-            api(it)
-        }
-
         extension.compileOnlyDependencies.forEach {
             compileOnlyApi(it)
-        }
-
-        extension.annotationProcessorDependencies.forEach {
-            annotationProcessor(it)
-        }
-
-        extension.modules.forEach {
-            api(project(it))
         }
     }
 }
