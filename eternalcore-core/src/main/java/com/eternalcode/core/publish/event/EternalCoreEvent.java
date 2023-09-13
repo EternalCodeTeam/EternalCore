98f6d1ebf403701c6dcad70fc74b162cc1dec738
Rollczi <ndejlich5@gmail.com>
2023-09-13 19:20:25 +0200

    Follow Jakubk15 request changes.

--- eternalcore-core/src/main/java/com/eternalcode/core/publish/event/EternalCoreEvent.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/publish/event/EternalCoreEvent.java
@@ -2,5 +2,5 @@ package com.eternalcode.core.publish.event;
 
 import com.eternalcode.core.publish.PublishEvent;
 
-public interface EternalEvent extends PublishEvent {
+public interface EternalCoreEvent extends PublishEvent {
 }
