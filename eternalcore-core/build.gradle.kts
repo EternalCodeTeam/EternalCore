085febc8ac091840bae128126d42d5b3de195e25
Rafał Chomczyk <rchomczyk@icloud.com>
2023-09-16 11:22:47 +0200

    Add a missing database driver for H2

--- eternalcore-core/build.gradle.kts
+++ eternalcore-core/build.gradle.kts
@@ -40,6 +40,7 @@ eternalShadow {
     // configuration
     library("org.mariadb.jdbc:mariadb-java-client:3.2.0")
     library("org.postgresql:postgresql:42.6.0")
+    library("com.h2database:h2:2.2.222")
     library("net.dzikoysk:cdn:1.14.4")
     libraryRelocate(
         "net.dzikoysk.cdn"
