8b10fd66c9b07fbee5aa331b83d91bc806d9d7b3
Osnixer <Osnixeroo@gmail.com>
2023-12-10 02:41:24 +0100

    Add TpaCommand to support ignored players

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/teleport/request/TpaCommand.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/teleport/request/TpaCommand.java
@@ -1,6 +1,7 @@
 package com.eternalcode.core.feature.teleport.request;
 
 import com.eternalcode.annotations.scan.command.DescriptionDocs;
+import com.eternalcode.core.feature.ignore.IgnoreService;
 import com.eternalcode.core.injector.annotations.Inject;
 import com.eternalcode.core.notice.NoticeService;
 import dev.rollczi.litecommands.argument.Arg;
@@ -8,17 +9,20 @@ import dev.rollczi.litecommands.command.execute.Execute;
 import dev.rollczi.litecommands.command.permission.Permission;
 import dev.rollczi.litecommands.command.route.Route;
 import org.bukkit.entity.Player;
+import panda.std.reactive.Completable;
 
 @Route(name = "tpa")
 @Permission("eternalcore.tpa")
 class TpaCommand {
 
     private final TeleportRequestService requestService;
+    private final IgnoreService ignoreService;
     private final NoticeService noticeService;
 
     @Inject
-    TpaCommand(TeleportRequestService requestService, NoticeService noticeService) {
+    TpaCommand(TeleportRequestService requestService, IgnoreService ignoreService, NoticeService noticeService) {
         this.requestService = requestService;
+        this.ignoreService = ignoreService;
         this.noticeService = noticeService;
     }
 
@@ -26,14 +30,12 @@ class TpaCommand {
     @DescriptionDocs(description = "Send teleport request to player", arguments = "<player>")
     void execute(Player player, @Arg Player target) {
         if (player.equals(target)) {
-
             this.noticeService.player(player.getUniqueId(), translation -> translation.tpa().tpaSelfMessage());
 
             return;
         }
 
         if (this.requestService.hasRequest(player.getUniqueId(), target.getUniqueId())) {
-
             this.noticeService.player(player.getUniqueId(), translation -> translation.tpa().tpaAlreadySentMessage());
 
             return;
@@ -46,14 +48,24 @@ class TpaCommand {
             .placeholder("{PLAYER}", target.getName())
             .send();
 
-        this.noticeService
-            .create()
-            .player(target.getUniqueId())
-            .notice(translation -> translation.tpa().tpaReceivedMessage())
-            .placeholder("{PLAYER}", player.getName())
-            .send();
+        this.isIgnoring(target, player).then((isIgnoring) -> {
+            if (isIgnoring) {
+                return;
+            }
+
+            this.noticeService
+                .create()
+                .player(target.getUniqueId())
+                .notice(translation -> translation.tpa().tpaReceivedMessage())
+                .placeholder("{PLAYER}", player.getName())
+                .send();
+        });
 
         this.requestService.createRequest(player.getUniqueId(), target.getUniqueId());
+    }
+
+    Completable<Boolean> isIgnoring(Player target, Player sender) {
+        return this.ignoreService.isIgnored(target.getUniqueId(), sender.getUniqueId());
     }
 }
 
