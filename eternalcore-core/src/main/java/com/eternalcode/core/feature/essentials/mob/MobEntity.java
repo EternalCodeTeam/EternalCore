458288601e3bc407320aba231199c9de08e65ab6
Michał Wojtas <80779749+CitralFlo@users.noreply.github.com>
2023-12-13 12:25:22 +0100

    GH-589 Add Vehicle and armor stand as MobTypes. Rename incorrect spelling of method. (#589)

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/mob/MobEntity.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/mob/MobEntity.java
@@ -4,6 +4,8 @@ import com.eternalcode.core.util.EntityUtil;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Monster;
+import org.bukkit.entity.Vehicle;
+import org.bukkit.entity.ArmorStand;
 
 class MobEntity {
 
@@ -23,6 +25,8 @@ class MobEntity {
         return switch (this.mobType) {
             case PASSIVE -> EntityUtil.is(entity, Animals.class);
             case AGGRESSIVE -> EntityUtil.is(entity, Monster.class);
+            case VEHICLE -> EntityUtil.is(entity, Vehicle.class);
+            case ARMOR_STAND -> EntityUtil.is(entity, ArmorStand.class);
             case OTHER -> EntityUtil.is(entity, this.entityClass);
             case ALL -> EntityUtil.isMob(entity);
         };
