34e5bc7f5d76196c3b9cc6203f6a3cbba1f8aed5
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-12-23 13:40:09 +0100

    Revert blossom.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("net.kyori:blossom:2.1.0")
+    implementation("net.kyori:blossom:1.3.1")
     implementation("com.github.johnrengelman:shadow:8.1.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.21")
     implementation("net.minecrell:plugin-yml:0.6.0")
