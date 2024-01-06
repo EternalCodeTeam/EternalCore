4c17ceeae34ecb6fe680f921e2eb0413da6372e8
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-01-06 23:00:27 +0100

    Refactor dependencies to use version variables

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,39 @@
+class Versions {
+
+    companion object {
+        const val SPIGOT_API = "1.19.4-R0.1-SNAPSHOT"
+        const val PAPER_API = "1.19.4-R0.1-SNAPSHOT"
+
+        const val JETBRAINS_ANNOTATIONS = "24.1.0"
+        const val PLACEHOLDER_API = "2.11.5"
+        const val LOMBOK = "1.18.20"
+        const val GIT_CHECK = "1.0.0"
+
+        const val PAPERLIB = "1.0.8"
+        const val ADVENTURE_PLATFORM = "4.3.2"
+        const val ADVENTURE_TEXT_MINIMESSAGE = "4.15.0"
+        const val CDN_CONFIGS = "1.14.4"
+
+        const val MARIA_DB = "3.3.2"
+        const val POSTGRESQL = "42.7.1"
+        const val H2 = "2.1.214"
+        const val ORMLITE = "6.1"
+        const val HIKARI_CP = "5.1.0"
+
+        const val LITE_COMMANDS = "3.2.2"
+        const val LITE_SKULL_API = "1.3.0"
+
+        const val GUAVA = "33.0.0-jre"
+        const val GSON = "2.10.1"
+
+        const val EXPRESSIBLE = "1.3.6"
+        const val PANDA_UTILITIES = "0.5.3-alpha"
+        const val APACHE_COMMONS = "2.15.1"
+
+        const val TRIUMPH_GUI = "3.1.7"
+
+        const val BSTATS = "3.0.2"
+        const val PIXEL_WIDTH = "1.1.0"
+    }
+
+}
--- eternalcore-api/build.gradle.kts
+++ eternalcore-api/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:${Versions.SPIGOT_API}")
 }
