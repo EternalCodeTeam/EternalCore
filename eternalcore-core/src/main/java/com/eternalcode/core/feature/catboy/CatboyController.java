c5a9590d45219028925a153fc6ff396309b56314
Rollczi <ndejlich5@gmail.com>
2024-02-04 00:57:28 +0100

    Add damage sounds.

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/catboy/CatboyController.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/catboy/CatboyController.java
@@ -4,11 +4,14 @@ import com.eternalcode.core.feature.teleport.event.EternalTeleportEvent;
 import com.eternalcode.core.injector.annotations.Inject;
 import com.eternalcode.core.injector.annotations.component.Controller;
 import com.eternalcode.core.scheduler.Scheduler;
+import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.entity.Cat;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.spigotmc.event.entity.EntityDismountEvent;
 
@@ -58,6 +61,27 @@ class CatboyController implements Listener {
 
             this.catboyService.unmarkAsCatboy(event.getPlayer());
             this.scheduler.laterSync(() -> this.catboyService.markAsCatboy(event.getPlayer(), catboy.selectedType()), TICK);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    void onHit(EntityDamageByEntityEvent event) {
+        if (!(event.getEntity() instanceof Player player)) {
+            return;
+        }
+
+        if (!(event.getDamager() instanceof Player damager)) {
+            return;
+        }
+
+        World world = player.getWorld();
+
+        if (this.catboyService.isCatboy(player.getUniqueId())) {
+            world.playSound(player.getLocation(), Sound.ENTITY_CAT_PURREOW, 1.0F, 1.0F);
+        }
+
+        if (this.catboyService.isCatboy(damager.getUniqueId())) {
+            world.playSound(damager.getLocation(), Sound.ENTITY_CAT_AMBIENT, 1.0F, 1.0F);
         }
     }
 
