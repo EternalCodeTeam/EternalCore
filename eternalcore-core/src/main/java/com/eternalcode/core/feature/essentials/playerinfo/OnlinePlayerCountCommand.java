cdeaf6bf057de32fe2f2b29802f5ef6426292115
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-02-18 18:52:08 +0100

    Don't count vanished players in list and online command.

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/playerinfo/OnlinePlayerCountCommand.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/playerinfo/OnlinePlayerCountCommand.java
@@ -1,6 +1,7 @@
 package com.eternalcode.core.feature.essentials.playerinfo;
 
 import com.eternalcode.annotations.scan.command.DescriptionDocs;
+import com.eternalcode.core.feature.vanish.VanishService;
 import com.eternalcode.core.injector.annotations.Inject;
 import com.eternalcode.core.notice.NoticeService;
 import com.eternalcode.core.viewer.Viewer;
@@ -8,30 +9,38 @@ import dev.rollczi.litecommands.annotations.context.Context;
 import dev.rollczi.litecommands.annotations.execute.Execute;
 import dev.rollczi.litecommands.annotations.permission.Permission;
 import dev.rollczi.litecommands.annotations.command.Command;
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.Server;
-
+import org.bukkit.entity.Player;
 
 @Command(name = "online")
 @Permission("eternalcore.online")
 class OnlinePlayerCountCommand {
 
     private final NoticeService noticeService;
+    private final VanishService vanishService;
     private final Server server;
 
     @Inject
-    OnlinePlayerCountCommand(NoticeService noticeService, Server server) {
+    OnlinePlayerCountCommand(NoticeService noticeService, VanishService vanishService, Server server) {
         this.noticeService = noticeService;
+        this.vanishService = vanishService;
         this.server = server;
     }
 
     @Execute
     @DescriptionDocs(description = "Shows online players count")
     void execute(@Context Viewer viewer) {
+        long visiblePlayerCount = this.server.getOnlinePlayers().stream()
+            .filter(player -> !this.vanishService.isVanished(player.getUniqueId()))
+            .count();
+
         this.noticeService
             .create()
             .notice(translation -> translation.player().onlinePlayersCountMessage())
             .viewer(viewer)
-            .placeholder("{ONLINE}", String.valueOf(this.server.getOnlinePlayers().size()))
+            .placeholder("{ONLINE}", String.valueOf(visiblePlayerCount))
             .send();
     }
 }
