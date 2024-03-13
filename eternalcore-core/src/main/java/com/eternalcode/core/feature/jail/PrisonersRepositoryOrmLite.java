1d475c9f9f97d00c1895daa4956713e661e0a4a6
CitralFlo <wojtas.michal90@gmail.com>
2024-03-13 19:23:54 +0100

    Move PrisonerWrapper to separate class

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/jail/PrisonersRepositoryOrmLite.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/jail/PrisonersRepositoryOrmLite.java
@@ -6,14 +6,9 @@ import com.eternalcode.core.database.wrapper.AbstractRepositoryOrmLite;
 import com.eternalcode.core.injector.annotations.Inject;
 import com.eternalcode.core.injector.annotations.component.Repository;
 
-import com.j256.ormlite.field.DataType;
-import com.j256.ormlite.field.DatabaseField;
-import com.j256.ormlite.table.DatabaseTable;
 import com.j256.ormlite.table.TableUtils;
 
 import java.sql.SQLException;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -72,43 +67,4 @@ class PrisonersRepositoryOrmLite extends AbstractRepositoryOrmLite implements Pr
         this.save(PrisonerWrapper.class, PrisonerWrapper.from(prisoner));
     }
 
-    @DatabaseTable(tableName = "eternal_core_prisoners")
-    static class PrisonerWrapper {
-
-        @DatabaseField(columnName = "id", id = true)
-        private UUID uuid;
-
-
-        @DatabaseField(columnName = "detained_at", dataType = DataType.SERIALIZABLE)
-        private Instant detainedAt;
-
-        @DatabaseField(columnName = "duration", dataType = DataType.SERIALIZABLE)
-        private Duration duration;
-
-        @DatabaseField(columnName = "detained_by")
-        private String detainedBy;
-
-        PrisonerWrapper() {
-        }
-
-        PrisonerWrapper(UUID uuid, Instant detainedAt, Duration duration, String detainedBy) {
-            this.uuid = uuid;
-            this.detainedAt = detainedAt;
-            this.duration = duration;
-            this.detainedBy = detainedBy;
-        }
-
-        Prisoner toPrisoner() {
-            return new Prisoner(this.uuid, this.detainedAt, this.duration, this.detainedBy);
-        }
-
-        static PrisonerWrapper from(Prisoner prisoner) {
-            return new PrisonerWrapper(
-                prisoner.getPlayerUniqueId(),
-                prisoner.getDetainedAt(),
-                prisoner.getDuration(),
-                prisoner.getDetainedBy()
-            );
-        }
-    }
 }
