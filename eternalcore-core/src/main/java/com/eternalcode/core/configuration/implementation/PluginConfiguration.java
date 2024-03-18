3f78f6736679dc9e933fa132c11ae8db022342a6
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-03-18 16:31:47 +0100

    GH-723 Add option to disable mark as afk. (#725)
    * Add option to disable mark as afk.
    
    * Follow mr. rollczi feedback.
    
    * Follow mr. rollczi feedback.
    
    * Reorder autoMarkAsAfk declaration in PluginConfiguration.
    
    * Rename to AutoAfk.

--- eternalcore-core/src/main/java/com/eternalcode/core/configuration/implementation/PluginConfiguration.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/configuration/implementation/PluginConfiguration.java
@@ -318,11 +318,23 @@ public class PluginConfiguration implements ReloadableConfig {
         @Description({ " ", "# Time before using the /afk command again" })
         public Duration afkCommandDelay = Duration.ofSeconds(60);
 
+        @Description({
+            "# Should a player be marked as AFK automatically?",
+            "# If set to true, the player will be marked as AFK after a certain amount of time of inactivity",
+            "# If set to false, the player will have to use the /afk command to be marked as AFK"
+        })
+        public boolean autoAfk = true;
+
         @Description({ " ", "# The amount of time a player must be inactive to be marked as AFK" })
         public Duration afkInactivityTime = Duration.ofMinutes(10);
 
         @Description({ " ", "# Should a player be kicked from the game when marked as AFK?" })
         public boolean kickOnAfk = false;
+
+        @Override
+        public boolean autoAfk() {
+            return this.autoAfk;
+        }
 
         @Override
         public int interactionsCountDisableAfk() {
