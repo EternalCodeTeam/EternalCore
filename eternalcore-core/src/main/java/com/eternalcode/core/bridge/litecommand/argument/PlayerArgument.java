b0ce65747fb611c533ddb4bc416c17db22e6801e
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-04-20 21:39:55 +0200

    GH-757 Resolve case sensitivity issue in command execution for Minecraft nicknames (#758)
    * GH-757 Resolve case sensitivity issue in command execution for Minecraft nicknames
    
    Signed-off-by: Martin <vluckyyy.biznes@gmail.com>
    
    * Update eternalcore-core/src/main/java/com/eternalcode/core/bridge/litecommand/argument/PlayerArgument.java
    
    Co-authored-by: DMK <81445555+imDMK@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Martin <vluckyyy.biznes@gmail.com>
    Co-authored-by: DMK <81445555+imDMK@users.noreply.github.com>

--- eternalcore-core/src/main/java/com/eternalcode/core/bridge/litecommand/argument/PlayerArgument.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/bridge/litecommand/argument/PlayerArgument.java
@@ -14,6 +14,7 @@ import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import java.util.Objects;
 
 @LiteArgument(type = Player.class)
 public class PlayerArgument extends AbstractViewerArgument<Player> {
@@ -28,7 +29,7 @@ public class PlayerArgument extends AbstractViewerArgument<Player> {
 
     @Override
     public ParseResult<Player> parse(Invocation<CommandSender> invocation, String argument, Translation translation) {
-        Player target = this.server.getPlayer(argument);
+        Player target = this.server.getPlayerExact(argument);
 
         if (target == null) {
             return ParseResult.failure(translation.argument().offlinePlayer());
