f62134a4087d8139f5a3a9c8d4a44646affafdaa
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2024-06-10 22:24:31 +0200

    Add more examples

--- eternalcore-api-example/src/main/java/com/eternalcode/example/feature/ignore/ApiIgnoreListener.java
+++ eternalcore-api-example/src/main/java/com/eternalcode/example/feature/ignore/ApiIgnoreListener.java
@@ -1,6 +1,9 @@
 package com.eternalcode.example.feature.ignore;
 
+import com.eternalcode.core.feature.ignore.event.IgnoreAllEvent;
 import com.eternalcode.core.feature.ignore.event.IgnoreEvent;
+import com.eternalcode.core.feature.ignore.event.UnIgnoreAllEvent;
+import com.eternalcode.core.feature.ignore.event.UnIgnoreEvent;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -22,16 +25,40 @@ public class ApiIgnoreListener implements Listener {
     }
 
     /**
+     * This method is called when a player ignores all players
+     **/
+
+    @EventHandler
+    public void onIgnoreAll(IgnoreAllEvent event) {
+        Player by = event.getBy();
+
+        by.playSound(by.getLocation(), Sound.ENTITY_VILLAGER_NO, 1.0F, 1.0F);
+        by.getWorld().strikeLightning(by.getLocation());
+    }
+
+    /**
      * This method is called when a player is unignored.
      **/
 
     @EventHandler
-    public void onUnIgnore(IgnoreEvent event) {
+    public void onUnIgnore(UnIgnoreEvent event) {
         Player by = event.getBy();
         Player target = event.getTarget();
 
         by.playSound(by.getLocation(), Sound.ENTITY_VILLAGER_YES, 1.0F, 1.0F);
         target.getWorld().strikeLightning(target.getLocation());
+    }
+
+    /**
+     * This method is called when a player unignores all players
+     **/
+
+    @EventHandler
+    public void onUnIgnoreAll(UnIgnoreAllEvent event) {
+        Player by = event.getBy();
+
+        by.playSound(by.getLocation(), Sound.BLOCK_PORTAL_TRIGGER, 1.0F, 1.0F);
+        by.getWorld().createExplosion(by.getLocation(), 2.0F);
     }
 
 }
