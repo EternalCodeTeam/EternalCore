66f63c74b4dc49034e3c30155106e292b43443d2
Martin <vluckyyy.biznes@gmail.com>
2024-06-18 22:39:02 +0200

    Refactor DurationUtil for formatting options

--- eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
@@ -7,23 +7,28 @@ import java.time.temporal.ChronoUnit;
 
 public class DurationUtil {
 
-    private static final TemporalAmountParser<Duration> WITHOUT_MILLS = new DurationParser()
+    private static final TemporalAmountParser<Duration> WITHOUT_MILLS_FORMAT = new DurationParser()
         .withUnit("s", ChronoUnit.SECONDS)
         .withUnit("m", ChronoUnit.MINUTES)
         .withUnit("h", ChronoUnit.HOURS)
         .withUnit("d", ChronoUnit.DAYS)
         .roundOff(ChronoUnit.MILLIS);
 
+    private static final TemporalAmountParser<Duration> STANDARD_FORMAT = new DurationParser()
+        .withUnit("s", ChronoUnit.SECONDS)
+        .withUnit("m", ChronoUnit.MINUTES)
+        .withUnit("h", ChronoUnit.HOURS)
+        .withUnit("d", ChronoUnit.DAYS);
+
     public DurationUtil() {
         throw new UnsupportedOperationException("This class cannot be instantiated");
     }
 
-
-    public static String format(Duration duration) {
-        if (duration.toMillis() < 1000) {
-            return "0s";
+    public static String format(Duration duration, boolean removeMills) {
+        if (removeMills) {
+            return WITHOUT_MILLS_FORMAT.format(duration);
         }
 
-        return WITHOUT_MILLS.format(duration);
+        return STANDARD_FORMAT.format(duration);
     }
 }
