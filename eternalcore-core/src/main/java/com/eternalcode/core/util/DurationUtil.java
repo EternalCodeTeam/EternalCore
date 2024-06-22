1af87be17d8e5d7415eee21f4cab3d4fb63bcdff
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-06-22 18:24:31 +0200

    GH-790 Fix duration format (#794)
    * Fix duration format which return empty text instead of 0s
    
    * Refactor DurationUtil for formatting options
    
    * Refactor DurationUtil for formatting options
    
    * Fix duration formatter.
    
    * Move to static constant.
    
    * Update eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
    
    Co-authored-by: Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
    
    * Update eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
    
    Co-authored-by: Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
    
    * Update eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
    
    Co-authored-by: Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
    
    * Update eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
    
    Co-authored-by: Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
    
    * Update eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
    
    Co-authored-by: Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jakubk15 <77227023+Jakubk15@users.noreply.github.com>

--- eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
@@ -7,18 +7,39 @@ import java.time.temporal.ChronoUnit;
 
 public class DurationUtil {
 
-    private static final TemporalAmountParser<Duration> WITHOUT_MILLS = new DurationParser()
+    private static final TemporalAmountParser<Duration> WITHOUT_MILLIS_FORMAT = new DurationParser()
         .withUnit("s", ChronoUnit.SECONDS)
         .withUnit("m", ChronoUnit.MINUTES)
         .withUnit("h", ChronoUnit.HOURS)
         .withUnit("d", ChronoUnit.DAYS)
         .roundOff(ChronoUnit.MILLIS);
 
+    private static final TemporalAmountParser<Duration> STANDARD_FORMAT = new DurationParser()
+        .withUnit("d", ChronoUnit.DAYS)
+        .withUnit("h", ChronoUnit.HOURS)
+        .withUnit("m", ChronoUnit.MINUTES)
+        .withUnit("s", ChronoUnit.SECONDS)
+        .withUnit("ms", ChronoUnit.MILLIS);
+
+    public static final Duration ONE_SECOND = Duration.ofSeconds(1);
+
     public DurationUtil() {
         throw new UnsupportedOperationException("This class cannot be instantiated");
     }
 
+    public static String format(Duration duration, boolean removeMillis) {
+        if (removeMillis) {
+            return WITHOUT_MILLIS_FORMAT.format(duration);
+        }
+
+        if (duration.toMillis() < ONE_SECOND.toMillis()) {
+            return STANDARD_FORMAT.format(duration);
+        }
+
+        return WITHOUT_MILLIS_FORMAT.format(duration);
+    }
+
     public static String format(Duration duration) {
-        return WITHOUT_MILLS.format(duration);
+        return format(duration, false);
     }
 }
