1d532c1fd0011f48fe96571ddf3ba1f4ec444806
Martin <vluckyyy.biznes@gmail.com>
2024-08-10 22:14:35 +0200

    Revert litecommands version.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -23,7 +23,7 @@ object Versions {
     const val ORMLITE = "6.1"
     const val HIKARI_CP = "5.1.0"
 
-    const val LITE_COMMANDS = "3.4.2-SNAPSHOT"
+    const val LITE_COMMANDS = "3.4.2"
     const val LITE_SKULL_API = "1.3.0"
 
     const val GUAVA = "33.2.1-jre"
