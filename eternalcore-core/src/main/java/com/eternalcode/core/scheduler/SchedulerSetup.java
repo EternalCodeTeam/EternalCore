6c6a5baa6ae3f597bc6817494215414a2baf313a
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-09-09 02:58:29 +0200

    [WIP] The EternalCore special test object has been initialized

--- eternalcore-core/src/main/java/com/eternalcode/core/scheduler/SchedulerSetup.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/scheduler/SchedulerSetup.java
@@ -1,6 +1,7 @@
 package com.eternalcode.core.scheduler;
 
 import com.eternalcode.commons.bukkit.scheduler.BukkitSchedulerImpl;
+import com.eternalcode.commons.folia.scheduler.FoliaSchedulerImpl;
 import com.eternalcode.commons.scheduler.Scheduler;
 import com.eternalcode.core.injector.annotations.Bean;
 import com.eternalcode.core.injector.annotations.component.BeanSetup;
@@ -10,8 +11,29 @@ import org.bukkit.plugin.Plugin;
 @BeanSetup
 public class SchedulerSetup implements Subscriber {
 
+    public static final String FOLIA = "io.papermc.paper.threadedregions.RegionizedServer";
+
     @Bean
     public Scheduler scheduler(Plugin plugin) {
-        return new BukkitSchedulerImpl(plugin);
+        return getAdaptiveScheduler(plugin);
+    }
+
+    public static Scheduler getAdaptiveScheduler(Plugin plugin) {
+        if (isFolia()) {
+            return new FoliaSchedulerImpl(plugin);
+        }
+        else {
+            return new BukkitSchedulerImpl(plugin);
+        }
+    }
+
+    private static boolean isFolia() {
+        try {
+            Class.forName(FOLIA);
+            return true;
+        }
+        catch (ClassNotFoundException exception) {
+            return false;
+        }
     }
 }
