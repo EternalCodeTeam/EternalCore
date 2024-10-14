f724ebd8fe5422ca53f4beed58a91bb8a3711598
Martin <vluckyyy.biznes@gmail.com>
2024-10-15 01:37:34 +0200

    Follow @Rollczi feedback.

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/item/give/GiveService.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/item/give/GiveService.java
@@ -4,13 +4,12 @@ import com.eternalcode.core.configuration.implementation.PluginConfiguration;
 import com.eternalcode.core.injector.annotations.Inject;
 import com.eternalcode.core.injector.annotations.component.Service;
 import com.eternalcode.core.notice.NoticeService;
+import com.eternalcode.core.util.InventoryUtil;
 import dev.triumphteam.gui.builder.item.ItemBuilder;
 import java.util.Optional;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 @Service
 public class GiveService {
@@ -29,64 +28,62 @@ public class GiveService {
     }
 
     public void giveItem(Player player, Material material) {
-        if (this.isInvalidMaterial(material, player)) {
-            return;
-        }
-
         this.giveItems(player, material, this.defaultGiveAmount, Optional.empty());
     }
 
     public void giveItem(Player player, Material material, int amount) {
-        if (this.isInvalidMaterial(material, player)) {
-            return;
-        }
-
         this.giveItems(player, material, amount, Optional.empty());
     }
 
     public void giveItem(Player player, Material material, int amount, Enchantment enchantment, int level) {
-        if (this.isInvalidMaterial(material, player)) {
-            return;
-        }
-
         this.giveItems(player, material, amount, Optional.of(new EnchantmentLevelPair(enchantment, level)));
     }
 
     private void giveItems(
-        Player player,
-        Material material,
-        int amount,
-        Optional<EnchantmentLevelPair> enchantmentLevel
+            Player player,
+            Material material,
+            int amount,
+            Optional<EnchantmentLevelPair> enchantmentLevel
     ) {
-        int maxStackSize = material.getMaxStackSize();
-        int fullStacks = amount / maxStackSize;
-        int remainder = amount % maxStackSize;
-
-        for (int i = 0; i < fullStacks; i++) {
-            this.giveItemStack(player, material, maxStackSize, enchantmentLevel);
+        if (this.isInvalidMaterial(material)) {
+            this.noticeService.create()
+                    .notice(translation -> translation.item().giveNotItem())
+                    .player(player.getUniqueId())
+                    .send();
+            return;
         }
 
-        if (remainder > 0) {
-            this.giveItemStack(player, material, remainder, enchantmentLevel);
-        }
+        this.giveItemStack(player, material, amount, enchantmentLevel);
     }
 
-    private boolean isInvalidMaterial(Material material, Player player) {
-        if (!material.isItem()) {
-            this.noticeService.create()
-                .notice(translation -> translation.item().giveNotItem())
-                .player(player.getUniqueId())
-                .send();
-            return true;
-        }
-        return false;
+    private boolean isInvalidMaterial(Material material) {
+        return !material.isItem();
     }
 
     private void giveItemStack(
-        Player player,
-        Material material,
-        int amount,
-        Optional<EnchantmentLevelPair> enchantmentLevel
+            Player player,
+            Material material,
+            int amount,
+            Optional<EnchantmentLevelPair> enchantmentLevel
+    ) {
+        int maxStackSize = material.getMaxStackSize();
+        int fullStacks = amount / maxStackSize;
+        int rest = amount % maxStackSize;
+
+        for (int i = 0; i < fullStacks; i++) {
+            this.giveSingleStack(player, material, maxStackSize, enchantmentLevel);
+        }
+
+        if (rest > 0) {
+            this.giveSingleStack(player, material, rest, enchantmentLevel);
+        }
+    }
+
+    private void giveSingleStack(
+            Player player,
+            Material material,
+            int amount,
+            Optional<EnchantmentLevelPair> enchantmentLevel
     ) {
         ItemBuilder itemBuilder = ItemBuilder.from(material).amount(amount);
 
@@ -98,41 +95,20 @@ public class GiveService {
 
         boolean dropOnFullInventory = this.pluginConfiguration.items.dropOnFullInventory;
 
-        if (!hasSpace(player.getInventory(), itemBuilder.build())) {
+        if (!InventoryUtil.hasSpace(player.getInventory(), itemBuilder.build())) {
             if (dropOnFullInventory) {
                 player.getWorld().dropItemNaturally(player.getLocation(), itemBuilder.build());
+                return;
             }
 
             this.noticeService.create()
-                .notice(translation -> translation.item().giveNoSpace())
-                .player(player.getUniqueId())
-                .send();
+                    .notice(translation -> translation.item().giveNoSpace())
+                    .player(player.getUniqueId())
+                    .send();
             return;
         }
 
         player.getInventory().addItem(itemBuilder.build());
-    }
-
-    private static boolean hasSpace(Inventory inventory, ItemStack itemStack) {
-        if (inventory.firstEmpty() != -1) {
-            return true;
-        }
-
-        for (ItemStack contents : inventory.getContents()) {
-            if (contents == null) {
-                continue;
-            }
-
-            if (!contents.isSimilar(itemStack)) {
-                continue;
-            }
-
-            if (contents.getMaxStackSize() > contents.getAmount()) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     private record EnchantmentLevelPair(Enchantment enchantment, int level) {
