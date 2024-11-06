34dcc57e328e21b28742e41d85bc8cd9620f5c01
Kacper <52548042+mydelkonivea@users.noreply.github.com>
2024-11-06 23:03:23 +0100

    Init commit

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/alert/AlertManager.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/essentials/alert/AlertManager.java
@@ -1,27 +1,29 @@
 package com.eternalcode.core.feature.essentials.alert;
 
+import com.eternalcode.commons.scheduler.Scheduler;
 import com.eternalcode.core.injector.annotations.Inject;
 import com.eternalcode.core.injector.annotations.component.Service;
 import com.eternalcode.core.notice.EternalCoreBroadcastImpl;
 import com.eternalcode.core.notice.NoticeTextType;
 import com.eternalcode.core.translation.Translation;
 import com.eternalcode.core.viewer.Viewer;
-import net.kyori.adventure.text.Component;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 class AlertManager {
-    //private final Map<UUID, Map<NoticeTextType, List<EternalCoreBroadcastImpl<Viewer, Translation, ?>>>> broadcasts = new HashMap<>();
+
     private final Map<AlertKey, List<EternalCoreBroadcastImpl<Viewer, Translation, ?>>> broadcasts = new HashMap<>();
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final Scheduler scheduler;
 
     @Inject
-    AlertManager() {
+    AlertManager(Scheduler scheduler) {
+        this.scheduler = scheduler;
     }
 
     void addBroadcast(UUID uuid, NoticeTextType type, EternalCoreBroadcastImpl<Viewer, Translation, ?> broadcast) {
@@ -38,6 +40,7 @@ class AlertManager {
 
     boolean removeLatestBroadcastWithType(UUID uuid, NoticeTextType type) {
         List<EternalCoreBroadcastImpl<Viewer, Translation, ?>> broadcastList = this.broadcasts.get(new AlertKey(uuid, type));
+
         if (broadcastList != null && !broadcastList.isEmpty()) {
             broadcastList.remove(broadcastList.size() - 1);
             if (broadcastList.isEmpty()) {
@@ -61,7 +64,7 @@ class AlertManager {
             if (type == NoticeTextType.TITLE || type == NoticeTextType.SUBTITLE || type == NoticeTextType.ACTIONBAR) {
                 for (int i = 0; i < broadcastList.size(); i++) {
                     EternalCoreBroadcastImpl<Viewer, Translation, ?> broadcast = broadcastList.get(i);
-                    scheduler.schedule(broadcast::send, i * 2L, TimeUnit.SECONDS);
+                    scheduler.runLater(broadcast::send, Duration.ofSeconds(i * 2L));
                 }
             } else {
                 for (EternalCoreBroadcastImpl<Viewer, Translation, ?> broadcast : broadcastList) {
