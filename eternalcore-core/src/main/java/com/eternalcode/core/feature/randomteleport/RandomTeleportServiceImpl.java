5e17616f1b7ae58e8a1a0a873d8d750a5e3af232
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-11-19 20:27:51 +0100

    Update eternalcore-core/src/main/java/com/eternalcode/core/feature/randomteleport/RandomTeleportServiceImpl.java
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/randomteleport/RandomTeleportServiceImpl.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/randomteleport/RandomTeleportServiceImpl.java
@@ -55,7 +55,7 @@ class RandomTeleportServiceImpl implements RandomTeleportService {
 
             if (world == null) {
                 throw new IllegalStateException(
-                    "World " + this.randomTeleportSettings.randomTeleportWorld() + " does not exists!");
+                    "World " + this.randomTeleportSettings.randomTeleportWorld() + " does not exist!");
             }
         }
 
