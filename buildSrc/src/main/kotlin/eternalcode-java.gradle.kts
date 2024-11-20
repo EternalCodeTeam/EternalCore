3365886fd921fa6d9b52cd844cbfe0a0af23ba96
Rollczi <ndejlich5@gmail.com>
2024-11-20 23:22:29 +0100

    Support new sound serdes after bukkit api update. Drop java 17 support (WiP)

--- buildSrc/src/main/kotlin/eternalcode-java.gradle.kts
+++ buildSrc/src/main/kotlin/eternalcode-java.gradle.kts
@@ -35,5 +35,5 @@ java {
 tasks.withType<JavaCompile>() {
     options.compilerArgs = listOf("-Xlint:deprecation", "-parameters")
     options.encoding = "UTF-8"
-    options.release = 17
+    options.release = 21
 }
--- eternalcore-plugin/build.gradle.kts
+++ eternalcore-plugin/build.gradle.kts
@@ -38,6 +38,6 @@ dependencies {
 
 tasks {
     runServer {
-        minecraftVersion("1.21.1")
+        minecraftVersion("1.21.3")
     }
 }
