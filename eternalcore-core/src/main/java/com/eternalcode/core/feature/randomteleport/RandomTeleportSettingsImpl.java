23e9971852b8b20c9648ac940d91a278b95df76a
Rollczi <ndejlich5@gmail.com>
2025-01-10 22:00:06 +0100

    Add Material.LEGACY_GRASS,

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/randomteleport/RandomTeleportSettingsImpl.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/randomteleport/RandomTeleportSettingsImpl.java
@@ -98,6 +98,7 @@ public class RandomTeleportSettingsImpl implements RandomTeleportSettings, Migra
         Material.ROSE_BUSH,
         Material.PEONY,
         Material.LARGE_FERN,
+        Material.LEGACY_GRASS,
         Material.LEGACY_LONG_GRASS,
         Material.LEGACY_DEAD_BUSH,
         Material.RAIL,
