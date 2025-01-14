a0c2ce3e9adcec7c3b60d71e533e0308c617b427
Rollczi <ndejlich5@gmail.com>
2025-01-15 00:23:45 +0100

    Simplify API

--- eternalcore-api/src/main/java/com/eternalcode/core/feature/language/LanguageService.java
+++ eternalcore-api/src/main/java/com/eternalcode/core/feature/language/LanguageService.java
@@ -1,6 +1,5 @@
 package com.eternalcode.core.feature.language;
 
-import java.util.Locale;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -15,10 +14,6 @@ public interface LanguageService {
     Language getLanguageNow(UUID player);
 
     CompletableFuture<Void> setLanguage(UUID player, Language language);
-
-    default void setLanguage(UUID player, Locale locale) {
-        this.setLanguage(player, Language.fromLocale(locale));
-    }
 
     CompletableFuture<Void> setDefaultLanguage(UUID player);
 
