3b110338ebbc18971d3baf754be2fa9ab32442a2
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2025-02-12 23:31:02 +0100

    Fix chat delay is not save to configuration.

--- eternalcore-core/src/main/java/com/eternalcode/core/configuration/composer/DurationComposer.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/configuration/composer/DurationComposer.java
@@ -1,22 +1,27 @@
 package com.eternalcode.core.configuration.composer;
 
-import dev.rollczi.litecommands.time.DurationParser;
-import panda.std.Result;
-
+import com.eternalcode.commons.time.DurationParser;
 import java.time.Duration;
 import java.time.format.DateTimeParseException;
-import java.util.Locale;
+import panda.std.Result;
 
 public class DurationComposer implements SimpleComposer<Duration> {
 
     @Override
     public Result<Duration, Exception> deserialize(String source) {
+        if (source.equals("0s")) {
+            return Result.ok(Duration.ZERO);
+        }
+
         return Result.supplyThrowing(DateTimeParseException.class, () -> DurationParser.TIME_UNITS.parse(source));
     }
 
     @Override
     public Result<String, Exception> serialize(Duration entity) {
+        if (entity.isZero()) {
+            return Result.ok("0s");
+        }
+
         return Result.ok(DurationParser.TIME_UNITS.format(entity));
     }
-
 }
