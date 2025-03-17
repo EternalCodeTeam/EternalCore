f4d518bef8bb211ea8a654e1004775f1a618ea9e
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2025-03-17 19:19:23 +0100

    Use class instead of interface

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/teleportrequest/TeleportRequestPermissionConstant.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/teleportrequest/TeleportRequestPermissionConstant.java
@@ -1,14 +1,14 @@
 package com.eternalcode.core.feature.teleportrequest;
 
-public interface TeleportRequestPermissionConstant {
+public class TeleportRequestPermissionConstant {
 
-    String TELEPORT_REQUEST_PERMISSION = "eternalcore.tpa";
-    String TELEPORT_REQUEST_ACCEPT_PERMISSION = "eternalcore.tpaccept";
-    String TELEPORT_REQUEST_DENY_PERMISSION = "eternalcore.tpdeny";
-    String TELEPORT_REQUEST_HERE_PERMISSION = "eternalcore.tpahere";
-    String TELEPORT_REQUEST_HERE_ACCEPT_PERMISSION = "eternalcore.tpahere.accept";
-    String TELEPORT_REQUEST_HERE_DENY_PERMISSION = "eternalcore.tpahere.deny";
+    public static final String TELEPORT_REQUEST_PERMISSION =             "eternalcore.tpa";
+    public static final String TELEPORT_REQUEST_ACCEPT_PERMISSION =      "eternalcore.tpaccept";
+    public static final String TELEPORT_REQUEST_DENY_PERMISSION =        "eternalcore.tpdeny";
+    public static final String TELEPORT_REQUEST_HERE_PERMISSION =        "eternalcore.tpahere";
+    public static final String TELEPORT_REQUEST_HERE_ACCEPT_PERMISSION = "eternalcore.tpahere.accept";
+    public static final String TELEPORT_REQUEST_HERE_DENY_PERMISSION =   "eternalcore.tpahere.deny";
 
-    String TELEPORT_TIMER_BYPASS_PERMISSION = "eternalcore.teleport.bypass";
+    public static final String TELEPORT_TIMER_BYPASS_PERMISSION =        "eternalcore.teleport.bypass";
 
 }
