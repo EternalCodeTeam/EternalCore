a65bb24d92db924e04a29b5c943c0610682c03e8
Igor Michalski <65646893+igoyek@users.noreply.github.com>
2025-03-28 23:07:28 +0100

    Fix jar build
    Took 34 minutes

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/signeditor/messages/PLSignEditorMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/signeditor/messages/PLSignEditorMessages.java
@@ -8,7 +8,7 @@ import net.dzikoysk.cdn.entity.Contextual;
 @Getter
 @Accessors(fluent = true)
 @Contextual
-public class PLSignEditorMessages implements SignEditorMessages{
+public class PLSignEditorMessages implements SignEditorMessages {
     public Notice noSignFound = Notice.chat("<red>✘ <dark_red>Spójrz na tabliczkę, aby ją edytować!");
     public Notice invalidIndex = Notice.chat("<red>✘ <dark_red>Wartość {LINE} jest nieprawidłowa! <red>Wybierz numer od 1 do 4!");
     public Notice lineSet = Notice.chat("<green>► <white>Wartość <green>{LINE}</green> została ustawiona na <green>{TEXT}");
