fe2466ee01cc66583f145489b3ccbceeea19c79a
Rollczi <ndejlich5@gmail.com>
2025-05-24 14:26:39 +0200

    Code review

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/signeditor/messages/PLSignEditorMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/signeditor/messages/PLSignEditorMessages.java
@@ -11,5 +11,5 @@ import net.dzikoysk.cdn.entity.Contextual;
 public class PLSignEditorMessages implements SignEditorMessages {
     public Notice noSignFound = Notice.chat("<red>✘ <dark_red>Spójrz na tabliczkę, aby ją edytować!");
     public Notice invalidIndex = Notice.chat("<red>✘ <dark_red>Wartość {LINE} jest nieprawidłowa! <red>Wybierz numer od 1 do 4!");
-    public Notice lineSet = Notice.chat("<green>► <white>Wartość <green>{LINE}</green> została ustawiona na <green>{TEXT}");
+    public Notice lineSet = Notice.chat("<green>► <white>Zmieniono linię <green>{LINE}</green> na <green>{TEXT}");
 }
