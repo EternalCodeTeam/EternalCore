56e885a40dd80d3e8d04257bfc314f6bb8523cc6
Piotr Zych <77621271+P1otrulla@users.noreply.github.com>
2025-07-11 02:03:21 +0200

    GH-1039 Add field scan for PermissionDocs like in VanishPermissionConstant (#1039)

--- eternalcore-docs-api/src/main/java/com/eternalcode/annotations/scan/SingleAnnotationScanResolver.java
+++ eternalcore-docs-api/src/main/java/com/eternalcode/annotations/scan/SingleAnnotationScanResolver.java
@@ -1,6 +1,7 @@
 package com.eternalcode.annotations.scan;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +42,14 @@ public abstract class SingleAnnotationScanResolver<A extends Annotation, RESULT>
 
             if (methodAnnotation != null) {
                 annotations.add(methodAnnotation);
+            }
+        }
+
+        for (Field field : classToScan.getDeclaredFields()) {
+            A fieldAnnotation = field.getAnnotation(annotationClass);
+
+            if (fieldAnnotation != null) {
+                annotations.add(fieldAnnotation);
             }
         }
 
