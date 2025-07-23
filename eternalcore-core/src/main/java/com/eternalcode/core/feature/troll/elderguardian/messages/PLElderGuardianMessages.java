3b750d6d2cdc09226ecbfba8b4d468681f723da2
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2025-07-23 11:52:22 +0200

    Remove @Contextual annotation from translation implementation

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/troll/elderguardian/messages/PLElderGuardianMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/troll/elderguardian/messages/PLElderGuardianMessages.java
@@ -3,11 +3,9 @@ package com.eternalcode.core.feature.troll.elderguardian.messages;
 import com.eternalcode.multification.notice.Notice;
 import lombok.Getter;
 import lombok.experimental.Accessors;
-import net.dzikoysk.cdn.entity.Contextual;
 
 @Getter
 @Accessors(fluent = true)
-@Contextual
 public class PLElderGuardianMessages implements ElderGuardianMessages {
 
     public Notice elderGuardianShown = Notice.chat("<green>► <white>Pokazano Elder Guardian'a graczowi <green>{PLAYER}<white>!");
