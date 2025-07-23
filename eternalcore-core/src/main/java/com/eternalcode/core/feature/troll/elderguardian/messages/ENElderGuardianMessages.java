7ba63050ee6cae4c1d3d7823d83532cd6caa5939
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2025-07-23 18:25:29 +0200

    Make appropriate messages implementations extend OkaeriConfig

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/troll/elderguardian/messages/ENElderGuardianMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/troll/elderguardian/messages/ENElderGuardianMessages.java
@@ -1,12 +1,13 @@
 package com.eternalcode.core.feature.troll.elderguardian.messages;
 
 import com.eternalcode.multification.notice.Notice;
+import eu.okaeri.configs.OkaeriConfig;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
 @Getter
 @Accessors(fluent = true)
-public class ENElderGuardianMessages implements ElderGuardianMessages {
+public class ENElderGuardianMessages extends OkaeriConfig implements ElderGuardianMessages {
 
     public Notice elderGuardianShown =
         Notice.chat("<green>► <white>Shown elder guardian to player <green>{PLAYER}<white>!");
