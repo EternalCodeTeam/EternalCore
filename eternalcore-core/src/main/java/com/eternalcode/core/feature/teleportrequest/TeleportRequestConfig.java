b69eb04b4e53396a42f8a818ceeb180b8b902aa9
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2025-08-03 00:59:46 +0200

    GH-1082 Move the config sections into dedicated classes. Improve `parent` comments of sections. (#1083)
    * GH-1082 Move the config sections into dedicated classes. Improve `parent` comments of sections.
    
    * Follow gemini code assistant review.
    
    * Follow @CitralFlo feedback.
    
    * Fix spelling.

---
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/teleportrequest/TeleportRequestConfig.java
@@ -0,0 +1,23 @@
+package com.eternalcode.core.feature.teleportrequest;
+
+import eu.okaeri.configs.OkaeriConfig;
+import eu.okaeri.configs.annotation.Comment;
+import java.time.Duration;
+
+public class TeleportRequestConfig extends OkaeriConfig implements TeleportRequestSettings {
+    @Comment("# Time of tpa requests expire")
+    public Duration tpaRequestExpire = Duration.ofSeconds(80);
+
+    @Comment("# Time of teleportation time in /tpa commands")
+    public Duration tpaTimer = Duration.ofSeconds(10);
+
+    @Override
+    public Duration teleportExpire() {
+        return this.tpaRequestExpire;
+    }
+
+    @Override
+    public Duration teleportTime() {
+        return this.tpaTimer;
+    }
+}
