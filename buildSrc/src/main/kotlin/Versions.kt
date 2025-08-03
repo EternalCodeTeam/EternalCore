64c018a007ae09b2d6a3ca416c0b62fbe1a08afc
vLuckyyy <vluckyyy.biznes@gmail.com>
2025-08-03 02:22:19 +0200

    Temponary fix: `Error sending packet clientbound/minecraft:set_action_bar_text`

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -13,7 +13,7 @@ object Versions {
 
     const val PAPERLIB = "1.0.8"
     const val ADVENTURE_TEXT_MINIMESSAGE = "4.24.0"
-    const val ADVENTURE_PLATFORM = "4.4.1"
+    const val ADVENTURE_PLATFORM = "4.4.0"
     const val OKAERI_CONFIGS = "5.0.9"
 
     const val MARIA_DB = "3.5.4"
