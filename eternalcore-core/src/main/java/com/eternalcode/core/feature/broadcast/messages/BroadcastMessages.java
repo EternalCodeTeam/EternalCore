1c5696c97eea846406695ef4f0a9d9e14a298714
Piotr Zych <77621271+P1otrulla@users.noreply.github.com>
2025-08-11 23:55:03 +0200

    remove unused messages

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/broadcast/messages/BroadcastMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/broadcast/messages/BroadcastMessages.java
@@ -1,16 +1,6 @@
 package com.eternalcode.core.feature.broadcast.messages;
 
-import com.eternalcode.multification.notice.Notice;
-
 public interface BroadcastMessages {
 
     String messageFormat();
-
-    Notice queueAdded();
-    Notice queueRemovedSingle();
-    Notice queueRemovedAll();
-    Notice queueCleared();
-    Notice queueEmpty();
-    Notice queueSent();
-
 }
--- eternalcore-core/src/main/java/com/eternalcode/core/feature/broadcast/messages/PLBroadcastMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/broadcast/messages/PLBroadcastMessages.java
@@ -1,6 +1,5 @@
 package com.eternalcode.core.feature.broadcast.messages;
 
-import com.eternalcode.multification.notice.Notice;
 import eu.okaeri.configs.OkaeriConfig;
 import eu.okaeri.configs.annotation.Comment;
 import lombok.Getter;
@@ -12,12 +11,5 @@ public class PLBroadcastMessages extends OkaeriConfig implements BroadcastMessag
 
     @Comment("# {BROADCAST} - Ogłoszenie")
     public String messageFormat = "<red><bold>OGŁOSZENIE:</bold> <gray>{BROADCAST}";
-
-    public Notice queueAdded = Notice.chat("<green>► <white>Dodano wiadomość do kolejki!");
-    public Notice queueRemovedSingle = Notice.chat("<green>► <white>Usunięto ostatnią wiadomość z kolejki!");
-    public Notice queueRemovedAll = Notice.chat("<green>► <white>Usunięto wszystkie wiadomości z kolejki!");
-    public Notice queueCleared = Notice.chat("<green>► <white>Wyczyszczono kolejkę wiadomości!");
-    public Notice queueEmpty = Notice.chat("<red>✘ <dark_red>Kolejka wiadomości jest pusta!");
-    public Notice queueSent = Notice.chat("<green>► <white>Wysłano wszystkie wiadomości z kolejki!");
 
 }
