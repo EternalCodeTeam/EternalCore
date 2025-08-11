53d981cb82248711062902c90e6ec49137311546
vLuckyyy <vluckyyy.biznes@gmail.com>
2025-08-12 00:16:14 +0200

    Add cache.

--- eternalcore-core/src/main/java/com/eternalcode/core/updater/UpdaterService.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/updater/UpdaterService.java
@@ -5,6 +5,9 @@ import com.eternalcode.commons.updater.Version;
 import com.eternalcode.commons.updater.impl.ModrinthUpdateChecker;
 import com.eternalcode.core.injector.annotations.Inject;
 import com.eternalcode.core.injector.annotations.component.Service;
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.time.Duration;
 import org.bukkit.plugin.PluginDescriptionFile;
 
 import java.util.concurrent.CompletableFuture;
@@ -13,19 +16,21 @@ import java.util.concurrent.CompletableFuture;
 class UpdaterService {
 
     private static final String MODRINTH_PROJECT_ID = "EternalCore";
+    private static final String CACHE_KEY = "modrinth-update";
 
-    private final ModrinthUpdateChecker updateChecker;
-    private final Version currentVersion;
+    private final AsyncLoadingCache<String, UpdateResult> updateCache;
 
     @Inject
     UpdaterService(PluginDescriptionFile pluginDescriptionFile) {
-        this.updateChecker = new ModrinthUpdateChecker();
-        this.currentVersion = new Version(pluginDescriptionFile.getVersion());
+        Version currentVersion = new Version(pluginDescriptionFile.getVersion());
+        ModrinthUpdateChecker updateChecker = new ModrinthUpdateChecker();
+
+        this.updateCache = Caffeine.newBuilder()
+            .expireAfterWrite(Duration.ofHours(1))
+            .buildAsync(key -> updateChecker.check(MODRINTH_PROJECT_ID, currentVersion));
     }
 
     CompletableFuture<UpdateResult> checkForUpdate() {
-        return CompletableFuture.supplyAsync(() ->
-            updateChecker.check(MODRINTH_PROJECT_ID, currentVersion)
-        );
+        return this.updateCache.get(CACHE_KEY);
     }
 }
