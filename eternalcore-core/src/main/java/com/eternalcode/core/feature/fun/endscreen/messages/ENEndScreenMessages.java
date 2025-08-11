76112b3afcc038ab5b8a1620dcbb99ead3fe0f2d
Piotr Zych <77621271+P1otrulla@users.noreply.github.com>
2025-08-12 00:27:52 +0200

    Added missing config comments

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/fun/endscreen/messages/ENEndScreenMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/fun/endscreen/messages/ENEndScreenMessages.java
@@ -2,6 +2,7 @@ package com.eternalcode.core.feature.fun.endscreen.messages;
 
 import com.eternalcode.multification.notice.Notice;
 import eu.okaeri.configs.OkaeriConfig;
+import eu.okaeri.configs.annotation.Comment;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
@@ -11,5 +12,7 @@ import lombok.experimental.Accessors;
 public class ENEndScreenMessages extends OkaeriConfig implements EndScreenMessages {
 
     public Notice shownToSelf = Notice.chat("<green>► <white>You have shown the end screen to yourself!</white>");
+
+    @Comment("# {PLAYER} - returns player's name")
     public Notice shownToOtherPlayer = Notice.chat("<green>► <white>You have shown the end screen to player <green>{PLAYER}!</green>");
 }
