17863fefeae17d6cf6e044d196125e54267f5be5
vLuckyyy <vluckyyy.biznes@gmail.com>
2025-08-12 03:56:44 +0200

    Follow mike's feedback.

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/teleportrandomplayer/messages/PLTeleportToRandomPlayerMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/teleportrandomplayer/messages/PLTeleportToRandomPlayerMessages.java
@@ -2,6 +2,7 @@ package com.eternalcode.core.feature.teleportrandomplayer.messages;
 
 import com.eternalcode.multification.notice.Notice;
 import eu.okaeri.configs.OkaeriConfig;
+import eu.okaeri.configs.annotation.Comment;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
@@ -12,12 +13,14 @@ public class PLTeleportToRandomPlayerMessages extends OkaeriConfig implements Te
     public Notice randomPlayerNotFound =
         Notice.chat("<red>✘ <dark_red>Nie można odnaleźć gracza do teleportacji!");
 
+    @Comment("{PLAYER} - Nazwa gracza, do którego zostałeś teleportowany")
     public Notice teleportedToRandomPlayer =
         Notice.chat("<green>► <white>Zostałeś losowo teleportowany do <green>{PLAYER}<white>!");
 
     public Notice randomPlayerInRangeNotFound =
         Notice.chat("<red>✘ <dark_red>Nie można odnaleźć gracza w zasięgu do teleportacji!");
 
+    @Comment("{PLAYER} - Nazwa gracza, do którego zostałeś teleportowany w zasięgu")
     public Notice teleportedToRandomPlayerInRange =
         Notice.chat("<green>► <white>Zostałeś losowo teleportowany do gracza w zasięgu: <green>{PLAYER}<white>!");
 }
