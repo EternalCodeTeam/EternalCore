462542e2e3ff489a50318b896340368acdb08b26
vLuckyyy <vluckyyy.biznes@gmail.com>
2025-08-16 00:37:16 +0200

    Fix migration name (0008) Migrations -> Migration

--- eternalcore-core/src/main/java/com/eternalcode/core/configuration/migrations/Migration_0008_Move_repair_to_dedicated_section.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/configuration/migrations/Migration_0008_Move_repair_to_dedicated_section.java
@@ -4,9 +4,9 @@ import eu.okaeri.configs.migrate.builtin.NamedMigration;
 
 import static eu.okaeri.configs.migrate.ConfigMigrationDsl.move;
 
-public class Migrations_0008_Move_repair_to_dedicated_section extends NamedMigration {
+public class Migration_0008_Move_repair_to_dedicated_section extends NamedMigration {
 
-    Migrations_0008_Move_repair_to_dedicated_section() {
+    Migration_0008_Move_repair_to_dedicated_section() {
         super(
             "Move repair to dedicated section",
             move("item.repairMessage", "repair.itemRepaired"),
--- eternalcore-core/src/main/java/com/eternalcode/core/configuration/migrations/Migrations.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/configuration/migrations/Migrations.java
@@ -10,7 +10,7 @@ public class Migrations {
         new Migration_0003_Move_tprp_to_dedicated_section(),
         new Migration_0006_Move_alert_to_broadcast_section(),
         new Migration_0007_Move_clear_to_dedicated_section(),
-        new Migrations_0008_Move_repair_to_dedicated_section()
+        new Migration_0008_Move_repair_to_dedicated_section()
     };
 
 }
