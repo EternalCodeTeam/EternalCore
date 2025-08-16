34efba9981e635b06f616515ae31be7b38bfdd13
Rollczi <ndejlich5@gmail.com>
2025-08-17 00:51:46 +0200

    Fix naming and regex in duration util and fix tests

--- eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/util/DurationUtil.java
@@ -4,13 +4,16 @@ import com.eternalcode.commons.time.DurationParser;
 import com.eternalcode.commons.time.TemporalAmountParser;
 import java.math.RoundingMode;
 import java.time.Duration;
+import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.regex.Pattern;
 
 public class DurationUtil {
 
     public static final Duration ONE_SECOND = Duration.ofSeconds(1);
-    private static final Pattern TOKEN = Pattern.compile("\\s*(\\d+)\\s*([a-zA-Z]+)\\s*");
+
+    private static final Pattern DURATION_REFORMAT_PATTERN = Pattern.compile("([0-9]+)([dhms]+)");
+    private static final String REPLACEMENT_WITH_SPACE = "$1$2 ";
 
     private static final TemporalAmountParser<Duration> WITHOUT_MILLIS_FORMAT = new DurationParser()
         .withUnit("s", ChronoUnit.SECONDS)
@@ -19,28 +22,28 @@ public class DurationUtil {
         .withUnit("d", ChronoUnit.DAYS)
         .withRounded(ChronoUnit.MILLIS, RoundingMode.UP);
     private static final TemporalAmountParser<Duration> STANDARD_FORMAT = new DurationParser()
-        .withUnit("d", ChronoUnit.DAYS)
-        .withUnit("h", ChronoUnit.HOURS)
-        .withUnit("m", ChronoUnit.MINUTES)
+        .withUnit("ms", ChronoUnit.MILLIS)
         .withUnit("s", ChronoUnit.SECONDS)
-        .withUnit("ms", ChronoUnit.MILLIS);
+        .withUnit("m", ChronoUnit.MINUTES)
+        .withUnit("h", ChronoUnit.HOURS)
+        .withUnit("d", ChronoUnit.DAYS);
 
     public DurationUtil() {
         throw new UnsupportedOperationException("This class cannot be instantiated");
     }
 
     public static String format(Duration duration, boolean removeMillis) {
-        String formatted;
         if (removeMillis) {
             if (duration.toMillis() < ONE_SECOND.toMillis()) {
                 return "0s";
             }
-            formatted = WITHOUT_MILLIS_FORMAT.format(duration);
+            return reformat(WITHOUT_MILLIS_FORMAT.format(duration));
         }
-        else {
-            formatted = STANDARD_FORMAT.format(duration);
-        }
-        return TOKEN.matcher(formatted).replaceAll("$1$2 ").trim();
+        return reformat(STANDARD_FORMAT.format(duration));
+    }
+
+    private static String reformat(String formatted) {
+        return DURATION_REFORMAT_PATTERN.matcher(formatted).replaceAll(REPLACEMENT_WITH_SPACE).trim();
     }
 
     public static String format(Duration duration) {
