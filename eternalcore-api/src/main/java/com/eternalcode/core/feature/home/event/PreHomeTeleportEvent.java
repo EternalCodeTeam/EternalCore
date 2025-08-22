6c38f6f70d37bfdf368f7bba2001b09f8caa41e9
Igor Michalski <65646893+igoyek@users.noreply.github.com>
2025-08-22 15:21:14 +0200

    Remove unused import
    Took 2 minutes

--- eternalcore-api/src/main/java/com/eternalcode/core/feature/home/event/PreHomeTeleportEvent.java
+++ eternalcore-api/src/main/java/com/eternalcode/core/feature/home/event/PreHomeTeleportEvent.java
@@ -3,7 +3,6 @@ package com.eternalcode.core.feature.home.event;
 import com.eternalcode.commons.bukkit.position.Position;
 import com.eternalcode.core.feature.home.Home;
 import java.util.UUID;
-import org.bukkit.Location;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
