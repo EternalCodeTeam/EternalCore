06927ef2b63f58dde61a3660841668016aa2eb8f
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2025-09-13 21:23:31 +0200

    Update runServer task to use Minecraft 1.21.8

--- eternalcore-plugin/build.gradle.kts
+++ eternalcore-plugin/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
 
 tasks {
     runServer {
-        minecraftVersion("1.21.7")
+        minecraftVersion("1.21.8")
         downloadPlugins.modrinth("luckperms", "v${Versions.LUCKPERMS}-bukkit")
     }
 }
