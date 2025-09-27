dcb1ca128477a591ed1bbe5d2e7f1aaa45f52c42
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2025-09-27 22:08:39 +0200

    Add support for Dynmap integration with vanish functionality.

--- eternalcore-core/src/main/java/com/eternalcode/core/bridge/BridgeManager.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/bridge/BridgeManager.java
@@ -1,11 +1,14 @@
 package com.eternalcode.core.bridge;
 
+import com.eternalcode.core.bridge.dynmap.DynmapBridgeInitializer;
 import com.eternalcode.core.bridge.placeholderapi.PlaceholderApiExtension;
 import com.eternalcode.core.bridge.placeholderapi.PlaceholderApiReplacer;
+import com.eternalcode.core.feature.vanish.VanishService;
 import com.eternalcode.core.placeholder.PlaceholderRegistry;
 import org.bukkit.Server;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
@@ -15,18 +18,32 @@ class BridgeManager {
     private final PluginDescriptionFile pluginDescriptionFile;
     private final Server server;
     private final Logger logger;
-    
-    BridgeManager(PlaceholderRegistry placeholderRegistry, PluginDescriptionFile pluginDescriptionFile, Server server, Logger logger) {
+    private final JavaPlugin plugin;
+    private final VanishService vanishService;
+
+    BridgeManager(PlaceholderRegistry placeholderRegistry,
+        PluginDescriptionFile pluginDescriptionFile,
+        Server server,
+        Logger logger,
+        JavaPlugin plugin,
+        VanishService vanishService
+    ) {
         this.placeholderRegistry = placeholderRegistry;
         this.pluginDescriptionFile = pluginDescriptionFile;
         this.server = server;
         this.logger = logger;
+        this.plugin = plugin;
+        this.vanishService = vanishService;
     }
 
     void init() {
         this.setupBridge("PlaceholderAPI", () -> {
             this.placeholderRegistry.registerPlaceholder(new PlaceholderApiReplacer());
             new PlaceholderApiExtension(this.placeholderRegistry, this.pluginDescriptionFile).initialize();
+        });
+
+        this.setupBridge("dynmap", () -> {
+            new DynmapBridgeInitializer(this.vanishService, this.plugin).initialize();
         });
     }
 
@@ -39,5 +56,4 @@ class BridgeManager {
             this.logger.info("Successfully hooked into " + pluginName + " bridge!");
         }
     }
-    
 }
--- eternalcore-core/src/main/java/com/eternalcode/core/bridge/BridgeManagerInitializer.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/bridge/BridgeManagerInitializer.java
@@ -1,21 +1,24 @@
 package com.eternalcode.core.bridge;
 
+import com.eternalcode.core.feature.vanish.VanishService;
 import com.eternalcode.core.injector.annotations.Bean;
 import com.eternalcode.core.injector.annotations.component.Setup;
 import com.eternalcode.core.placeholder.PlaceholderRegistry;
 import java.util.logging.Logger;
 import org.bukkit.Server;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
 
 @Setup
 class BridgeManagerInitializer {
 
     @Bean
-    BridgeManager bridgeManager(PlaceholderRegistry registry, Server server, Logger logger, PluginDescriptionFile description) {
-        BridgeManager bridgeManager = new BridgeManager(registry, description, server, logger);
+    BridgeManager bridgeManager(
+        PlaceholderRegistry registry, Server server, Logger logger,
+        PluginDescriptionFile description, JavaPlugin plugin, VanishService vanishService) {
+        BridgeManager bridgeManager = new BridgeManager(registry, description, server, logger, plugin, vanishService);
         bridgeManager.init();
 
         return bridgeManager;
     }
-
 }
