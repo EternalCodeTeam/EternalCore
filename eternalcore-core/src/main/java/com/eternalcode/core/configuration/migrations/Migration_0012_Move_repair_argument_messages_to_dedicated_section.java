6209a523770be39c2c68d4ddf1d906875dda21ec
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2025-09-30 21:31:50 +0200

    Fix migration description.

--- eternalcore-core/src/main/java/com/eternalcode/core/configuration/migrations/Migration_0012_Move_repair_argument_messages_to_dedicated_section.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/configuration/migrations/Migration_0012_Move_repair_argument_messages_to_dedicated_section.java
@@ -7,7 +7,7 @@ import eu.okaeri.configs.migrate.builtin.NamedMigration;
 public class Migration_0012_Move_repair_argument_messages_to_dedicated_section extends NamedMigration {
     Migration_0012_Move_repair_argument_messages_to_dedicated_section() {
         super(
-            "Move repair notices to dedicated section",
+            "Move repair argument messages to dedicated section",
             move("argument.noDamaged", "repair.cannotRepair"),
             move("argument.noDamagedItems", "repair.needDamagedItem")
         );
