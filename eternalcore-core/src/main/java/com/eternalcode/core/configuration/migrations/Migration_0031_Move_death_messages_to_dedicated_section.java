918509a12dd9db65b16f7182e3e36e8b59e5973e
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2025-11-08 17:40:45 +0100

    Update okaeri configs to 6.0.0-beta.3. Fix migration (0031)

--- eternalcore-core/src/main/java/com/eternalcode/core/configuration/migrations/Migration_0031_Move_death_messages_to_dedicated_section.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/configuration/migrations/Migration_0031_Move_death_messages_to_dedicated_section.java
@@ -10,9 +10,7 @@ public class Migration_0031_Move_death_messages_to_dedicated_section extends Nam
             "Move death messages to dedicated section",
             move("event.deathMessage", "deathMessage.playerKilledByOtherPlayer"),
             move("event.deathMessageByDamageCause", "deathMessage.playerDiedByDamageCause"),
-            move("event.unknownDeathCause", "deathMessage.playerDiedByUnknownCause"),
-            move("event.deathMessageByEntity", "deathMessage.playerKilledByEntity"),
-            move("event.deathMessageByMobType", "deathMessage.playerKilledByMobType")
+            move("event.unknownDeathCause", "deathMessage.playerDiedByUnknownCause")
         );
     }
 }
