3c3dbde97333a46f11260ac0d658c6aa100ab6fc
Michał Wojtas <80779749+CitralFlo@users.noreply.github.com>
2025-12-06 19:34:58 +0100

    GH-1147 Save User entity to database (#1147)
    * Add database for users, add batch fetching, update tests
    
    * Codestyle fixes
    
    * Resolve gemini review
    
    * Resolve @sadcenter and @Jakubk15 reviews
    
    * wip
    
    * wip2
    
    * wip
    
    * Update methods and failsafe for cache
    
    * Refactor `UserManager` and related classes to improve database integration, remove unused methods, and streamline cache handling.
    
    * Remove MySQL and Testcontainers integration, refactor database configuration, and streamline ORM functionality.
    
    * CR
    
    * Remove `created` and `lastLogin` fields from `User`, cleanup related methods, and simplify `UserManager` logic.
    
    * Remove `IntegrationTestSpec` and `TestScheduler` utilities as they are no longer used.
    
    * Add `lastSeen` and `accountCreated` fields to `User`, enhance `UserManager` logic, introduce date formatting utility, and update WHOIS command and messages accordingly.
    
    * Update `UserTable` to use `DATE_STRING` for `lastSeen` and `accountCreated` fields instead of `SERIALIZABLE`
    
    * Update `UserTable` to replace `Instant` with `Date` fields and downgrade Minecraft version in `runServer` task
    
    ---------
    
    Co-authored-by: Rollczi <ndejlich5@gmail.com>
    Co-authored-by: Martin Sulikowski <vluckyyy.biznes@gmail.com>

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/whois/PLWhoIsMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/whois/PLWhoIsMessages.java
@@ -9,28 +9,31 @@ import lombok.experimental.Accessors;
 @Getter
 @Accessors(fluent = true)
 public class PLWhoIsMessages extends OkaeriConfig implements WhoIsMessages {
-    
+
     @Comment({
-        " ",
-        "# {PLAYER} - nazwa gracza",
-        "# {UUID} - UUID gracza",
-        "# {IP} - IP gracza",
-        "# {WALK-SPEED} - prędkość chodzenia gracza",
-        "# {SPEED} - prędkość latania gracza",
-        "# {PING} - ping gracza",
-        "# {LEVEL} - poziom gracza",
-        "# {HEALTH} - zdrowie gracza",
-        "# {FOOD} - poziom najedzenia gracza"
+            " ",
+            "# {PLAYER} - nazwa gracza",
+            "# {UUID} - UUID gracza",
+            "# {IP} - IP gracza",
+            "# {WALK-SPEED} - prędkość chodzenia gracza",
+            "# {SPEED} - prędkość latania gracza",
+            "# {PING} - ping gracza",
+            "# {LEVEL} - poziom gracza",
+            "# {HEALTH} - zdrowie gracza",
+            "# {FOOD} - poziom najedzenia gracza",
+            "# {LAST-SEEN} - Ostatnio widziany",
+            "# {ACCOUNT-CREATED} - Data utworzenia konta"
     })
     public List<String> info = List.of(
-        "<color:#9d6eef>► <white>Gracz: <color:#9d6eef>{PLAYER}",
-        "<color:#9d6eef>► <white>UUID: <color:#9d6eef>{UUID}",
-        "<color:#9d6eef>► <white>IP: <color:#9d6eef>{IP}",
-        "<color:#9d6eef>► <white>Szybkość chodzenia: <color:#9d6eef>{WALK-SPEED}",
-        "<color:#9d6eef>► <white>Szybkość latania: <color:#9d6eef>{SPEED}",
-        "<color:#9d6eef>► <white>Opóźnienie: <color:#9d6eef>{PING}<white>ms",
-        "<color:#9d6eef>► <white>Poziom: <color:#9d6eef>{LEVEL}",
-        "<color:#9d6eef>► <white>Zdrowie: <color:#9d6eef>{HEALTH}",
-        "<color:#9d6eef>► <white>Poziom najedzenia: <color:#9d6eef>{FOOD}"
-    );
+            "<color:#9d6eef>► <white>Gracz: <color:#9d6eef>{PLAYER}",
+            "<color:#9d6eef>► <white>UUID: <color:#9d6eef>{UUID}",
+            "<color:#9d6eef>► <white>IP: <color:#9d6eef>{IP}",
+            "<color:#9d6eef>► <white>Szybkość chodzenia: <color:#9d6eef>{WALK-SPEED}",
+            "<color:#9d6eef>► <white>Szybkość latania: <color:#9d6eef>{SPEED}",
+            "<color:#9d6eef>► <white>Opóźnienie: <color:#9d6eef>{PING}<white>ms",
+            "<color:#9d6eef>► <white>Poziom: <color:#9d6eef>{LEVEL}",
+            "<color:#9d6eef>► <white>Zdrowie: <color:#9d6eef>{HEALTH}",
+            "<color:#9d6eef>► <white>Poziom najedzenia: <color:#9d6eef>{FOOD}",
+            "<color:#9d6eef>► <white>Ostatnio widziany: <green>{LAST-SEEN}",
+            "<color:#9d6eef>► <white>Data utworzenia konta: <green>{ACCOUNT-CREATED}");
 }
