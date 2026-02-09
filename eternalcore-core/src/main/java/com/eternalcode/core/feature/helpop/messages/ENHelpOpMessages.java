76bf7ab4dd12d79fc02a47e1cf1ea307960396d4
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2026-02-09 21:14:37 +0100

    feat(helpop): add admin reply messages and translations for helpop functionality

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/helpop/messages/ENHelpOpMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/helpop/messages/ENHelpOpMessages.java
@@ -13,17 +13,22 @@ import org.bukkit.Sound;
 public class ENHelpOpMessages extends OkaeriConfig implements HelpOpSection {
     @Comment("# {PLAYER} - Player who send message on /helpop, {TEXT} - message")
     Notice format = Notice.chat("<dark_gray>[<color:#9d6eef>HelpOp<dark_gray>] <white>{PLAYER}<dark_gray>: <white>{TEXT}");
+
     @Comment(" ")
     Notice send = Notice.chat("<color:#9d6eef>► <white>This message has been successfully sent to administration");
+
     @Comment("# {TIME} - Time to next use (cooldown)")
     Notice helpOpDelay = Notice.chat("<red>✘ <dark_red>You can use this command for: <red>{TIME}");
+
     @Comment("# {ADMIN} - Admin who replied, {PLAYER} - Player who received the reply, {TEXT} - Reply message")
     Notice adminReply = BukkitNotice.builder()
         .chat("<dark_gray>[<color:#9d6eef>HelpOp<dark_gray>] <white>{ADMIN} <dark_gray>-> <white>{PLAYER}<dark_gray>: <white>{TEXT}")
         .sound(Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f)
         .build();
+
     @Comment(" ")
     Notice adminReplySend = Notice.chat("<color:#9d6eef>► <white>Reply has been sent to player <color:#9d6eef>{PLAYER}");
+
     @Comment("# {PLAYER} - Player name")
     Notice playerNotSentHelpOp = Notice.chat("<red>✘ <dark_red>Player <red>{PLAYER} <dark_red>has not sent any helpop request");
 }
--- eternalcore-core/src/main/java/com/eternalcode/core/feature/helpop/messages/PLHelpOpMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/helpop/messages/PLHelpOpMessages.java
@@ -14,17 +14,22 @@ public class PLHelpOpMessages extends OkaeriConfig implements HelpOpSection {
     @Comment({ "# {PLAYER} - Gracz który wysłał wiadomość na helpop, {TEXT} - Treść wysłanej wiadomości" })
     Notice format = Notice
             .chat("<dark_gray>[<color:#9d6eef>HelpOp<dark_gray>] <white>{PLAYER}<dark_gray>: <white>{TEXT}");
+
     @Comment(" ")
     Notice send = Notice.chat("<color:#9d6eef>► <white>Wiadomość została wysłana do administracji");
+
     @Comment("# {TIME} - Czas do końca blokady (cooldown)")
     Notice helpOpDelay = Notice.chat("<red>✘ <dark_red>Możesz użyć tej komendy dopiero za <red>{TIME}<dark_red>!");
+
     @Comment("# {ADMIN} - Admin który odpowiedział, {PLAYER} - Gracz który otrzymał odpowiedź, {TEXT} - Treść odpowiedzi")
     Notice adminReply = BukkitNotice.builder()
         .chat("<dark_gray>[<color:#9d6eef>HelpOp<dark_gray>] <white>{ADMIN} <dark_gray>-> <white>{PLAYER}<dark_gray>: <white>{TEXT}")
         .sound(Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f)
         .build();
+
     @Comment(" ")
     Notice adminReplySend = Notice.chat("<color:#9d6eef>► <white>Odpowiedź została wysłana do gracza <color:#9d6eef>{PLAYER}");
+
     @Comment("# {PLAYER} - Nazwa gracza")
     Notice playerNotSentHelpOp = Notice.chat("<red>✘ <dark_red>Gracz <red>{PLAYER} <dark_red>nie wysłał żadnego zapytania na helpop");
 }
