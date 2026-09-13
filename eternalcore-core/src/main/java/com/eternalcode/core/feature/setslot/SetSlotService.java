e355579c65c0d8f4b7400e252c98c7a5cfaaac7b
igoyek <65646893+igoyek@users.noreply.github.com>
2026-09-13 22:17:35 +0200

    fix(setslot): replace NMS refleciton with Paper API, fix wrong validation message
    Took 19 minutes

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/setslot/SetSlotService.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/setslot/SetSlotService.java
@@ -2,72 +2,25 @@ package com.eternalcode.core.feature.setslot;
 
 import com.eternalcode.core.injector.annotations.Inject;
 import com.eternalcode.core.injector.annotations.component.Service;
-import com.eternalcode.core.util.ReflectUtil;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import org.bukkit.Server;
-import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.Blocking;
 
 @Service
 public class SetSlotService {
 
-    private final Plugin plugin;
-    private Field maxPlayersField;
+    private final Server server;
     private final SetSlotSaver setSlotSaver;
 
     @Inject
-    public SetSlotService(Plugin plugin, SetSlotSaver setSlotSaver) {
-        this.plugin = plugin;
+    public SetSlotService(Server server, SetSlotSaver setSlotSaver) {
+        this.server = server;
         this.setSlotSaver = setSlotSaver;
     }
 
     @Blocking
-    public void setCapacity(int slots) {
-        Object playerList = getPlayerList();
-
-        if (this.maxPlayersField == null) {
-            this.maxPlayersField = findMaxPlayersField(playerList);
-        }
-
-        this.setMaxPlayers(playerList, slots);
+    public void setCapacity(int slots){
+        this.server.setMaxPlayers(slots);
         this.setSlotSaver.save();
-    }
-
-    private Object getPlayerList() {
-        Server server = this.plugin.getServer();
-        Method serverGetHandle = ReflectUtil.getDeclaredMethod(server.getClass(), "getHandle");
-
-        return ReflectUtil.invokeMethod(serverGetHandle, server);
-    }
-
-    private Field findMaxPlayersField(Object playerList) {
-        Class<?> playerListClass = playerList.getClass().getSuperclass();
-
-        for (Field field : playerListClass.getDeclaredFields()) {
-            if (field.getType() == int.class) {
-                field.setAccessible(true);
-                try {
-                    if (field.getInt(playerList) == this.plugin.getServer().getMaxPlayers()) {
-                        return field;
-                    }
-                }
-                catch (IllegalAccessException exception) {
-                    throw new IllegalStateException("Error accessing field value for maxPlayers", exception);
-                }
-            }
-        }
-
-        throw new IllegalStateException("Unable to find maxPlayers field in " + playerListClass.getName());
-    }
-
-    private void setMaxPlayers(Object playerList, int slots) {
-        try {
-            this.maxPlayersField.setInt(playerList, slots);
-        }
-        catch (IllegalAccessException exception) {
-            throw new IllegalStateException("Failed to set maxPlayers field", exception);
-        }
     }
 }
 
