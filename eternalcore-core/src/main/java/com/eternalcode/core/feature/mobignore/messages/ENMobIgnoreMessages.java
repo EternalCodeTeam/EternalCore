cf2d9d6b60ce421598530fc95af33f628ceb50fc
Michał Wojtas <wojtas.michal90@gmail.com>
2026-09-16 18:25:49 +0200

    Use lower case letters in "You"

--- eternalcore-core/src/main/java/com/eternalcode/core/feature/mobignore/messages/ENMobIgnoreMessages.java
+++ eternalcore-core/src/main/java/com/eternalcode/core/feature/mobignore/messages/ENMobIgnoreMessages.java
@@ -9,8 +9,8 @@ import lombok.experimental.Accessors;
 @Getter
 @Accessors(fluent = true)
 public class ENMobIgnoreMessages extends OkaeriConfig implements MobIgnoreMessages {
-    Notice mobIgnore = Notice.chat("<color:#9d6eef>► <white>Monsters will no longer target You!");
-    Notice noMobIgnore = Notice.chat("<color:#9d6eef>► <white>Monsters will target You!");
+    Notice mobIgnore = Notice.chat("<color:#9d6eef>► <white>Monsters will no longer target you!");
+    Notice noMobIgnore = Notice.chat("<color:#9d6eef>► <white>Monsters will target you!");
 
     @Comment({ " ", "# Placeholder: {PLAYER} - Targeted player name" })
     Notice mobIgnoreTarget = Notice.chat("<color:#9d6eef>► <white>Monsters will ignore <green>{PLAYER}<white> - protection has been turned on!");
