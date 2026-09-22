e79d83d6bdaa122a62bbc03840fb64f83f74e8a4
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2026-09-22 21:54:47 +0200

    refactor: remove unused DependentComponent and RequiredService classes

--- eternalcore-core/src/test/java/com/eternalcode/core/feature/testrequired/RequiredService.java
+++
@@ -1,7 +0,0 @@
-package com.eternalcode.core.feature.testrequired;
-
-import com.eternalcode.core.modules.ModuleDescription;
-
-@ModuleDescription("Test fixture module used by ModuleServiceTest.")
-public interface RequiredService {
-}
